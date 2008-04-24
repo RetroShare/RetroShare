4e03fa5d06af07ec7bf742937cb91b11bd752383
vinnyx <unknown@unknown.com>
2008-04-24 12:26:44 +0000

    #ifndef protection global pointer to interface LGPL header
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/trunk@517 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- libretroshare/src/rsiface/rsstatus.h
+++ libretroshare/src/rsiface/rsstatus.h
@@ -1,3 +1,35 @@
+#ifndef RS_STATUS_INTERFACE_H
+#define RS_STATUS_INTERFACE_H
+
+/*
+ * libretroshare/src/rsiface: rsstatus.h
+ *
+ * RetroShare C++ Interface.
+ *
+ * Copyright 2007-2008 by Vinny Do.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License Version 2 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA.
+ *
+ * Please report all bugs and problems to "retroshare@lunamutt.com".
+ *
+ */
+
+class RsStatusInterface;
+
+extern RsStatusInterface *rsStatusInterface;
+
 #include <string.h>
 #include <inttypes.h>
 
@@ -8,6 +40,7 @@ const uint32_t RS_STATUS_ONLINE  = 0x0004;
 
 class StatusInfo
 {
+	public:
 	std::string id;
 	uint32_t status;
 };
@@ -19,3 +52,5 @@ virtual bool getStatus(std::string id, StatusInfo& statusInfo) = 0;
 virtual bool setStatus(StatusInfo& statusInfo)                 = 0;
 
 };
+
+#endif
