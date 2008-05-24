fe22d0b6519e91ef55c47dd957716f28aaea06fa
chrisparker126 <retroshare.project@gmail.com>
2008-05-24 15:10:59 +0000

    final interface, before starting on connectivity
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/trunk@561 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- libretroshare/src/rsiface/rsQblog.h
+++ libretroshare/src/rsiface/rsQblog.h
@@ -42,75 +42,75 @@ extern RsQblog *rsQblog;
  	public:
  	
  	
-	RsQblog() { return; }
-virtual ~RsQblog() { return; }
- 	
- 	/**
- 	 * allow user to set his status
- 	 * @param status The status of the user 
- 	 */
- 	virtual bool setStatus(std::string &status) = 0;
- 	
- 	/**
- 	 * get status of users friends
- 	 *
- 	 **/
- 	 virtual bool getStatus(std::string &status) = 0;
- 	 
- 	/**
- 	 * choose whether to filter or not
- 	 * @param filterSwitch
- 	 **/
- 	virtual bool setFilterSwitch(bool &filterSwitch) = 0;
- 	
- 	/**
- 	 * get usrs friend list
- 	 * @ param usrList
- 	 */
- 	 virtual bool getFriendList(std::list<std::string> &friendList) =0;
- 	
- 	/**
- 	 * retrieve usrs filterSwitch status
- 	 **/
- 	virtual bool getFilterSwitch(void) = 0;
- 	
- 	/**
- 	 * add user id to filter list 
- 	 * @param usr id to add to filter list
- 	 **/
- 	virtual bool addToFilter(std::string &usrId) = 0;
- 	
- 	/**
- 	 * remove friend from filter list
- 	 * @param id The user's frined's id
- 	 **/
- 	 virtual bool removeFiltFriend(std::string &usrId) = 0;
- 	 
- 	/**
- 	  * get users fav song
- 	  * @param usrId the usr whose fav song you want
- 	  * @param favSong puts ref for fav song here
- 	  */
- 	  virtual bool getProfile(std::string &usrId, std::string &favSong) = 0;
- 	  
- 	  
- 	  
- 	  /**
- 	   * for now just fav song, TODO: must find way to link to rs profile
- 	   */
- 	   virtual bool setProfile(std::string &favSong) = 0;
- 	   
- 	   /**
- 	    * send blog info, will send to a data structure for transmission
- 	    * @param msg The msg the usr wants to send
- 	    */
- 	   virtual bool sendBlog(std::string &msg) = 0;
- 	   
- 	   /**
- 	    * retrieve blog of a usr
- 	    * @param blogs contains the blog msgs of usr along with time posted for sorting
- 	    */
- 	   virtual bool getBlogs(std::map< std::string, std::multimap<long int, std:: string> > &blogs) = 0;
+		RsQblog() { return; }
+		virtual ~RsQblog() { return; }
+	 	
+	 	/**
+	 	 * allows user to set his status
+	 	 * @param status The status of the user 
+	 	 */
+	 	virtual bool setStatus(const std::string &status) = 0;
+	 	
+	 	/**
+	 	 * returns reference to map of usrs and their status
+	 	 * @param usrStatus returns map to usr and their status
+	 	 */
+	 	virtual bool getStatus(std::map<std::string, std::string> &usrStatus) = 0;
+	 	 
+	 	/**
+	 	 * choose whether to filter or not
+	 	 * @param filterSwitch
+	 	 */
+	 	virtual bool setFilterSwitch(bool &filterSwitch) = 0;
+	 	
+	 	/**
+	 	 * get usrs friend list
+	 	 * @ param usrList
+	 	 */
+	 	virtual bool getFriendList(std::list<std::string> &friendList) =0;
+	 	
+	 	/**
+	 	 * retrieve usrs filterSwitch status
+	 	 */
+	 	virtual bool getFilterSwitch(void) = 0;
+	 	
+	 	/**
+	 	 * add user id to filter list 
+	 	 * @param usr id to add to filter list
+	 	 */
+	 	virtual bool addToFilter(std::string &usrId) = 0;
+	 	
+	 	/**
+	 	 * remove friend from filter list
+	 	 * @param id The user's frined's id
+	 	 */
+	 	virtual bool removeFiltFriend(std::string &usrId) = 0;
+	 	 
+	 	/**
+	 	 * get users fav song
+	 	 * @param usrId the usr whose fav song you want
+	 	 * @param favSong puts ref for fav song here
+	 	 */
+	 	virtual bool getProfile(std::map<std::string, std::string> &profile) = 0;
+	 	  
+	 	  
+	 	  
+	 	/**
+	 	 * for now just fav song, TODO: must find way to link to rs profile
+	 	 */
+	 	virtual bool setProfile(const std::string &favSong) = 0;
+	 	   
+	 	/**
+	 	 * send blog info, will send to a data structure for transmission
+	     * @param msg The msg the usr wants to send
+	     */
+	    virtual bool sendBlog(const std::string &msg) = 0;
+	 	   
+	 	/**
+	 	  * retrieve blog of a usr
+	 	  * @param blogs contains the blog msgs of usr along with time posted for sorting
+	 	  */
+	 	virtual bool getBlogs(std::map< std::string, std::multimap<long int, std:: string> > &blogs) = 0;
  	   
  };
 
