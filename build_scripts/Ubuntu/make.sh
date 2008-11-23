c2b87a4af230e23324187008ebc30f6131449a31
csoler <retroshare.project@gmail.com>
2008-11-23 14:00:25 +0000

    updated build scripts for ubuntu
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/trunk@835 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- build_scripts/Ubuntu/make.sh
+++ build_scripts/Ubuntu/make.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-version="0.4.10b"
+###################### PARAMETERS ####################
+version="0.4.11b"
 arch=`dpkg --print-architecture`
+packager="Cyril Soler <csoler@users.sourceforge.net>"
+######################################################
 
 echo
 echo Building retroshare debian package version $version for Ubuntu $arch. 
@@ -11,6 +14,7 @@ echo "    "- you have compiled libretroshare and retroshare-gui in ../../libretr
 echo "                                                        "and ../../retroshare-gui/src/
 echo "    "- you have updated version numbers in ../../retroshare-gui/src/util/rsversion.cpp
 echo "                                      "and ../../retroshare-gui/src/retroshare.nsi
+echo "    "- version and name will be: RetroShare_"$version"_ubuntu_hardy_"$arch".deb
 
 if ! test `whoami` = "root" ; then
 	echo Please run this script as root.
@@ -22,15 +26,20 @@ echo
 echo Press [ENTER] when ok, else quit with Ctrl+C.
 read tmp
 
+echo
+echo Unzipping...
 tar zxvf retroshare.tgz
+echo Changing ownership...
 chown -R root.root retroshare 
 
 # setup version numbers and arch in DEBIAN/control
 
-cat retroshare/DEBIAN/control | sed -e s/XXXXXX/"$version"/g | sed -e s/YYYYYY/"$arch"/g > retroshare/DEBIAN/control.tmp
+echo Setting up version numbers...
+cat retroshare/DEBIAN/control | sed -e s/XXXXXX/"$version"/g | sed -e s/YYYYYY/"$arch"/g | sed -e s/ZZZZZZ/"$packager"/g > retroshare/DEBIAN/control.tmp
 mv retroshare/DEBIAN/control.tmp retroshare/DEBIAN/control
 
 # clean
+echo Cleaning...
 find retroshare -name "*~" -exec \rm -f {} \;
 
 # copy executables at the right place
@@ -44,14 +53,23 @@ if ! test -f ../../libretroshare/src/rsiface/retroshare-nogui; then
 	echo
 	exit ;
 fi
+echo Stripping executables...
 cp ../../retroshare-gui/src/RetroShare              retroshare/usr/bin/
+strip retroshare/usr/bin/RetroShare
 cp ../../libretroshare/src/rsiface/retroshare-nogui retroshare/usr/bin/
+strip retroshare/usr/bin/retroshare-nogui
 
 # compute md5 sums
+echo Computing/setting md5 sums...
 cd retroshare
 find usr -type f -exec md5sum {} \; > DEBIAN/md5sums
 cd ..
+echo Creating package RetroShare_"$version"_ubuntu_hardy_"$arch".deb
+
 dpkg-deb -b retroshare RetroShare_"$version"_ubuntu_hardy_"$arch".deb
 
 # cleaning
+echo Cleaning...
 \rm -rf retroshare
+
+echo Done.
