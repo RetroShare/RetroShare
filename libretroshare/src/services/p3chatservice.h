70e23ba9b4aabdb38fcfd9040319fbb98d0a376c
csoler <retroshare.project@gmail.com>
2009-06-18 22:09:16 +0000

    - changed save/load of avatars to derive from p3Config to improve code consistency and robustness - added RsChatAvatarItem and used it to exchange avatars. This should increase windows/linux compatibility - kept backward compatibility with old msg-based avatar format.
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/branches/v0.4.x@1306 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- libretroshare/src/services/p3chatservice.h
+++ libretroshare/src/services/p3chatservice.h
@@ -39,7 +39,9 @@
 #include "services/p3service.h"
 #include "pqi/p3connmgr.h"
 
-class p3ChatService: public p3Service, public pqiConfig
+#define AVATAR_KEEP_BACKWRD_COMP
+
+class p3ChatService: public p3Service, public p3Config
 {
 	public:
 		p3ChatService(p3ConnectMgr *cm);
@@ -64,9 +66,10 @@ class p3ChatService: public p3Service, public pqiConfig
 
 		std::list<RsChatMsgItem *> getChatQueue(); 
 
-		/*** Overloaded from pqiConfig ****/
-		virtual bool    loadConfiguration(std::string &loadHash);
-		virtual bool    saveConfiguration();
+		/************* from p3Config *******************/
+		virtual RsSerialiser *setupSerialiser() ;
+		virtual std::list<RsItem*> saveList(bool& cleanup) ;
+		virtual bool loadList(std::list<RsItem*> load) ;
 
 	private:
 		RsMutex mChatMtx;
@@ -77,12 +80,15 @@ class p3ChatService: public p3Service, public pqiConfig
 		void sendAvatarJpegData(const std::string& peer_id) ;
 
 		/// Receive the avatar in a chat item, with RS_CHAT_RECEIVE_AVATAR flag.
-		void receiveAvatarJpegData(RsChatMsgItem *ci) ;
+#ifdef AVATAR_KEEP_BACKWRD_COMP
+		void receiveAvatarJpegData(RsChatMsgItem *ci) ;	// old method
+#endif
+		void receiveAvatarJpegData(RsChatAvatarItem *ci) ;	// new method
 
 		/// Sends a request for an avatar to the peer of given id
 		void sendAvatarRequest(const std::string& peer_id) ;
 
-		RsChatMsgItem *makeOwnAvatarItem() ;
+		RsChatAvatarItem *makeOwnAvatarItem() ;
 
 		p3ConnectMgr *mConnMgr;
 
