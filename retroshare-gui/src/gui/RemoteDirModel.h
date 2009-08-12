caf1c7850f8ea26c3c0aac0a100c28211d10339d
alexandrut <alexandrut@users.sourceforge.net>
2009-08-12 19:20:32 +0000

    resolve compile problem
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/trunk@1524 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- retroshare-gui/src/gui/RemoteDirModel.h
+++ retroshare-gui/src/gui/RemoteDirModel.h
@@ -15,9 +15,9 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor,
  *  Boston, MA  02110-1301, USA.
- ****************************************************************/v
+ ****************************************************************/
 
 #ifndef REMOTE_DIR_MODEL
 #define REMOTE_DIR_MODEL
@@ -26,7 +26,7 @@
 #include <vector>
 #include <list>
 #include <string>
-#include <rsiface/rsfiles.h>
+#include "rsiface/rsfiles.h"
 #include "util/misc.h"
 #include "rsiface/rstypes.h"
 
