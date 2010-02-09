40e6e6ff8f74198ba0c776c83ee832e3261d7af4
joss17 <joss17@users.sourceforge.net>
2010-02-09 19:10:58 +0000

    add back the git part of the version script
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/trunk@2262 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- libretroshare/src/version_detail.sh
+++ libretroshare/src/version_detail.sh
@@ -3,6 +3,20 @@
 #don't exit even if a command fails
 set +e
 
+
+if ( git log -n 1 &> /dev/null); then
+	#retrieve git information
+	version="git : $(git status | grep branch | cut -c 13-) $(git log -n 1 | grep commit | cut -c 8-)"
+fi
+
+if ( git log -n 1 | grep svn &> /dev/null); then
+	#retrieve git svn information
+	version="$version  svn : $(git log -n 1 | grep svn | awk '{print $2}' | head -1 | sed 's/.*@//')"
+elif ( git log -n 10 | grep svn &> /dev/null); then
+	#retrieve git svn information
+	version="$version  svn closest version : $(git log -n 10 | grep svn | awk '{print $2}' | head -1 | sed 's/.*@//')"
+fi
+
 if ( svn info &> /dev/null); then
 	version=$(svn info | head -n 5 | tail -1)
 fi
