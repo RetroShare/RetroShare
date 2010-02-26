15a28b1d2ebacb652b42ed566d388ce8c31290d9
csoler <retroshare.project@gmail.com>
2010-02-26 12:38:29 +0000

    added a spinbox to limit the number of search results (too many results may hang the gui)
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/trunk@2432 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- retroshare-gui/src/gui/SearchDialog.cpp
+++ retroshare-gui/src/gui/SearchDialog.cpp
@@ -653,6 +653,16 @@ void SearchDialog::searchKeywords()
 
 void SearchDialog::updateFiles(qulonglong search_id,FileDetail file)
 {
+	static std::map<qulonglong,uint32_t> nb_results ;
+
+	if(nb_results.find(search_id) == nb_results.end())
+		nb_results[search_id] = 0 ;
+
+	if(nb_results[search_id] >= ui._max_results_SB->value())
+		return ;
+	else
+		++nb_results[search_id] ;
+
 	/* which extensions do we use? */
 	std::string txt = ui.lineEdit->text().toStdString();
 #ifdef DEBUG
