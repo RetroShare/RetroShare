936f0f88cb2b3253a255cdce43ae06dffc9492f5
joss17 <joss17@users.sourceforge.net>
2010-03-12 23:09:17 +0000

    put UPNPBase under LGPL
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/trunk@2525 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- libretroshare/src/upnp/UPnPBase.h
+++ libretroshare/src/upnp/UPnPBase.h
@@ -3,28 +3,26 @@
 //
 // Copyright (c) 2004-2009 Marcelo Roberto Jimenez ( phoenix@amule.org )
 // Copyright (c) 2006-2009 aMule Team ( admin@amule.org / http://www.amule.org )
+// Copyright (c) 2009-2010 Retroshare Team
 //
-// Any parts of this program derived from the xMule, lMule or eMule project,
-// or contributed by third-party developers are copyrighted by their
-// respective authors.
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation; either version 2 of the License, or
 // (at your option) any later version.
+// You are also granted to use it with the LGPL License
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
+//
+// You should have received a copy of the GNU General Public License or the LGPL
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA
 //
 
-
-//this file uses libupnp
+//This file uses libupnp
 
 #include <map>
 #include <string>
