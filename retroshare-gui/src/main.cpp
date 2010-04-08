c30e0c071ec1209b1564742fd8dfeb11030b11b6
chrisparker126 <retroshare.project@gmail.com>
2010-04-08 15:51:10 +0000

    added gpg initialization for autologin - your friends are not authenticated if not
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/trunk@2692 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- retroshare-gui/src/main.cpp
+++ retroshare-gui/src/main.cpp
@@ -116,8 +116,18 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		// true: note auto-login is active
-		RsInit::LoadCertificates(true);
+
+            std::string preferredId, gpgId, gpgName, gpgEmail, sslName;
+            RsInit::getPreferedAccountId(preferredId);
+
+            if (RsInit::getAccountDetails(preferredId,
+                            gpgId, gpgName, gpgEmail, sslName))
+            {
+                    RsInit::SelectGPGAccount(gpgId);
+            }
+
+            // true: note auto-login is active
+            RsInit::LoadCertificates(true);
 	}
 
 	rsicontrol->StartupRetroShare();
