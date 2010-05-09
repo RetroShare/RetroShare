8c9c08a28c01c805784ca8a40f2386d20838345a
defnax <retroshare.project@gmail.com>
2010-05-09 11:11:08 +0000

    update version
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/branches/v0.5.0@2874 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- retroshare-gui/src/util/rsversion.h
+++ retroshare-gui/src/util/rsversion.h
@@ -20,8 +20,8 @@
  ****************************************************************/
 
 
-#define GUI_VERSION "0.5.0a"
-#define GUI_REVISION "Revision: 2762  date : 23:28:41 04.22.10"
+#define GUI_VERSION "0.5.0b"
+#define GUI_REVISION "Build: 2862"
 
 
 #include <QString>
