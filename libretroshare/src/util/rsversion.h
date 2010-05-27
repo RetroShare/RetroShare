2f2f7610439160f1449f68491c3019fadff9c479
defnax <retroshare.project@gmail.com>
2010-05-27 23:54:58 +0000

    change rs revision number
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/trunk@3014 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- libretroshare/src/util/rsversion.h
+++ libretroshare/src/util/rsversion.h
@@ -7,8 +7,8 @@
 
 #include <string>
 
-#define LIB_VERSION "0.5.0"
-#define SVN_REVISION "Revision: 2854"
+#define LIB_VERSION "0.5.1"
+#define SVN_REVISION "$Revision: 2973$"
 
 namespace RsUtil {
 
