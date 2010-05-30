aafcf99c362b494cd7333c200f0e8f5eb71a33f2
csoler <retroshare.project@gmail.com>
2010-05-30 14:23:14 +0000

    updated scripts to automatically setup version numbers in the source code
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/branches/v0.5.0@3036 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- build_scripts/Ubuntu_src/make.sh
+++ build_scripts/Ubuntu_src/make.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
 
+svn=`svn up | cut -d\  -f3 | cut -d. -f1`
+
 echo This script will
 echo   - remove the directory retroshare-0.5/
 echo   - remove existing sources packages in the current directory
 echo   - build a new source package from the svn
 echo   - rebuild the source package for the karmic i386 arch.
+echo   - svn number is $svn. Is this correct ?
 echo 
-echo Type ^C to abort, or enter the svn version number to continue
-read svn
+echo Type ^C to abort, or type enter to proceed.
 
-# rm -rf ./retroshare-0.5
-# ./clean.sh
-# ./makeSourcePackage.sh
+rm -rf ./retroshare-0.5
+./clean.sh
+./makeSourcePackage.sh
 
-#sudo PBUILDFOLDER=/var/cache/pbuilder pbuilder-dist lucid i386 build *.dsc
-#cp /var/cache/pbuilder/lucid-i386_result/retroshare_0.5-1_i386.deb ./RetroShare_0.5."$svn"_lucid_i386.deb
-#
-#sudo PBUILDFOLDER=/var/cache/pbuilder pbuilder-dist lucid build *.dsc
-#cp /var/cache/pbuilder/lucid_result/retroshare_0.5-1_amd64.deb ./RetroShare_0.5."$svn"_lucid_amd64.deb
-#
-#sudo PBUILDFOLDER=/var/cache/pbuilder pbuilder-dist karmic build *.dsc
-#cp /var/cache/pbuilder/karmic_result/retroshare_0.5-1_amd64.deb ./RetroShare_0.5."$svn"_karmic_amd64.deb
+sudo PBUILDFOLDER=/var/cache/pbuilder pbuilder-dist lucid i386 build *.dsc
+cp /var/cache/pbuilder/lucid-i386_result/retroshare_0.5-1_i386.deb ./RetroShare_0.5."$svn"_lucid_i386.deb
+
+sudo PBUILDFOLDER=/var/cache/pbuilder pbuilder-dist lucid build *.dsc
+cp /var/cache/pbuilder/lucid_result/retroshare_0.5-1_amd64.deb ./RetroShare_0.5."$svn"_lucid_amd64.deb
+
+sudo PBUILDFOLDER=/var/cache/pbuilder pbuilder-dist karmic build *.dsc
+cp /var/cache/pbuilder/karmic_result/retroshare_0.5-1_amd64.deb ./RetroShare_0.5."$svn"_karmic_amd64.deb
 
 sudo PBUILDFOLDER=/var/cache/pbuilder pbuilder-dist karmic i386 build *.dsc
 cp /var/cache/pbuilder/karmic-i386_result/retroshare_0.5-1_i386.deb ./RetroShare_0.5."$svn"_karmic_i386.deb
@@ -28,9 +30,10 @@ cp /var/cache/pbuilder/karmic-i386_result/retroshare_0.5-1_i386.deb ./RetroShare
 sudo PBUILDFOLDER=/var/cache/pbuilder pbuilder-dist jaunty build *.dsc
 cp /var/cache/pbuilder/jaunty_result/retroshare_0.5-1_amd64.deb ./RetroShare_0.5."$svn"_jaunty_amd64.deb
 
-#sudo PBUILDFOLDER=/var/cache/pbuilder pbuilder-dist jaunty i386 build *.dsc
-#cp /var/cache/pbuilder/jaunty-i386_result/retroshare_0.5-1_i386.deb ./RetroShare_0.5."$svn"_jaunty_i386.deb
+sudo PBUILDFOLDER=/var/cache/pbuilder pbuilder-dist jaunty i386 build *.dsc
+cp /var/cache/pbuilder/jaunty-i386_result/retroshare_0.5-1_i386.deb ./RetroShare_0.5."$svn"_jaunty_i386.deb
 
 
 mv /media/disc2/csoler/RetroShare_Releases/*.deb /media/disc2/csoler/RetroShare_Releases/Old/
 mv *.deb /media/disc2/csoler/RetroShare_Releases/
+
--- build_scripts/Ubuntu_src/makeSourcePackage.sh
+++ build_scripts/Ubuntu_src/makeSourcePackage.sh
@@ -5,11 +5,12 @@ if test -d "RetroShare" ;  then
 	exit
 fi
 
-echo Please enter the svn number:
-read svnnumber
-
+svnnumber=`svn up | cut -d\  -f3 | cut -d. -f1`
 version=0.5.0."$svnnumber"
 packages="."
+
+echo SVN number is $svnnumber
+echo version is $version
 
 tar zxvf $packages/BaseRetroShareDirs.tgz
 
@@ -23,6 +24,15 @@ cd retroshare-0.5/src/retroshare-gui/
 svn co https://retroshare.svn.sourceforge.net/svnroot/retroshare/branches/v0.5.0/retroshare-gui/src . 
 #tar zxvf ../../../retroshare-gui.tgz
 cd ../../..
+
+# setup version numbers
+cat retroshare-0.5/src/libretroshare/util/rsversion.h | grep -v SVN_REVISION > /tmp/toto2342
+echo \#define SVN_REVISION \"Revision: "$svnnumber"  date : `date`\" >> /tmp/toto2342
+cp /tmp/toto2342 retroshare-0.5/src/libretroshare/util/rsversion.h
+
+cat retroshare-0.5/src/retroshare-gui/util/rsversion.h | grep -v GUI_REVISION > /tmp/toto4463
+echo \#define GUI_REVISION \"Revision: "$svnnumber"  date : `date`\" >> /tmp/toto4463
+cp /tmp/toto4463 retroshare-0.5/src/retroshare-gui/util/rsversion.h
 
 # Various cleaning
 
