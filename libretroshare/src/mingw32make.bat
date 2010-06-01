ca56bee92129fea4d6f9d9c60d663885a359885e
defnax <retroshare.project@gmail.com>
2010-06-01 22:46:56 +0000

    added svn_revision.bat file to update easier the rsversion.h under Windows, and modifyd the mingw32make.bat before compiling write the latest revision to rsversion.h
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/trunk@3049 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- libretroshare/src/mingw32make.bat
+++ libretroshare/src/mingw32make.bat
@@ -1,3 +1,11 @@
-mingw32-make -f Makefile %1
-if errorlevel 1 goto end
-:end
+set QTDIR=d:\qt\2010.01
+set MINGW=%QTDIR%\mingw
+
+set PATH=%QTDIR%\qt\bin;%QTDIR%\bin;%MINGW%\bin;%PATH%
+
+"D:\Programme\TortoiseSVN\bin\SubWCRev" . util\rsversion.in util\rsversion.h 
+
+qmake libretroshare.pro
+
+mingw32-make
+
