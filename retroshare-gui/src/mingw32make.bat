b30c142592b331453e0602eb57d31333668fa72c
defnax <retroshare.project@gmail.com>
2010-06-01 23:03:12 +0000

    added for windows compile and revision bat files, and template rsversion.in
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/trunk@3050 b45a01b8-16f6-495d-af2f-9b41ad6348cc

---
+++ retroshare-gui/src/mingw32make.bat
@@ -0,0 +1,11 @@
+set QTDIR=d:\qt\2010.01
+set MINGW=%QTDIR%\mingw
+
+set PATH=%QTDIR%\qt\bin;%QTDIR%\bin;%MINGW%\bin;%PATH%
+
+"D:\Programme\TortoiseSVN\bin\SubWCRev" . util\rsversion.in util\rsversion.h 
+
+qmake RetroShare.pro
+
+mingw32-make
+
