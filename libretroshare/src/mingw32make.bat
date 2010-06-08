79010a15f732da1b889823038ff2cb9b1a23e661
defnax <retroshare.project@gmail.com>
2010-06-08 22:24:00 +0000

    modified mingw32make.bat do make clean before start to compile
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/trunk@3088 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- libretroshare/src/mingw32make.bat
+++ libretroshare/src/mingw32make.bat
@@ -3,7 +3,9 @@ set MINGW=%QTDIR%\mingw
 
 set PATH=%QTDIR%\qt\bin;%QTDIR%\bin;%MINGW%\bin;%PATH%
 
-"D:\Programme\TortoiseSVN\bin\SubWCRev" . util\rsversion.in util\rsversion.h 
+"D:\Programme\TortoiseSVN\bin\SubWCRev" . util\rsversion.in util\rsversion.h
+
+mingw32-make clean 
 
 qmake libretroshare.pro
 
