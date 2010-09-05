2011d897eef46ad10fbd84c7773137921d5585c9
thunder2 <retroshare.project@gmail.com>
2010-09-05 00:14:42 +0000

    These damn new files.
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/trunk@3444 b45a01b8-16f6-495d-af2f-9b41ad6348cc

---
+++ retroshare-gui/src/gui/im_history/IMHistoryItemPainter.cpp
@@ -0,0 +1,64 @@
+/****************************************************************
+ *  RetroShare is distributed under the following license:
+ *
+ *  Copyright (C) 2009 The RetroShare Team
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA  02110-1301, USA.
+ ****************************************************************/
+
+#include <QTextDocument>
+#include <QPainter>
+#include <QAbstractTextDocumentLayout>
+
+#include "IMHistoryItemPainter.h"
+
+IMHistoryItemPainter::IMHistoryItemPainter(QString text)
+{
+    itemText = text;
+
+    /* calcultate size */
+    QTextDocument doc;
+    doc.setHtml(itemText);
+    size = doc.size().toSize();
+}
+
+QSize IMHistoryItemPainter::sizeHint() const
+{
+    return size;
+}
+
+void IMHistoryItemPainter::paint(QPainter *painter, const QStyleOptionViewItem &option, EditMode mode) const
+{
+//    if (mode == Editable) {
+//        painter->setBrush(option.palette.highlight());
+//    } else {
+        painter->setBrush(option.palette.foreground());
+//    }
+    if (option.state & QStyle::State_Selected) {
+        painter->fillRect(option.rect, option.palette.highlight());
+    }
+
+    painter->save();
+
+    QTextDocument doc;
+    doc.setHtml(itemText);
+    QAbstractTextDocumentLayout::PaintContext context;
+    doc.setPageSize(option.rect.size());
+    painter->translate(option.rect.x(), option.rect.y());
+    doc.documentLayout()->draw(painter, context);
+
+    painter->restore();
+}
