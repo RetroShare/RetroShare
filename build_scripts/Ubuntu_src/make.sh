dd19278d58803167f0a7fbe8c2a00698f7bf5784
csoler <retroshare.project@gmail.com>
2010-10-28 19:50:53 +0000

    fixed ubuntu package compilation scripts
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/trunk@3712 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- build_scripts/Ubuntu_src/make.sh
+++ build_scripts/Ubuntu_src/make.sh
@@ -9,18 +9,19 @@ echo
 
 echo attempting to get svn revision number...
 svn=`svn info | grep 'Revision:' | cut -d\  -f2`
+echo Revision number is $svn. 
 
 echo Type ^C to abort, or enter to continue
-read
+read tmp
 
 # rm -rf ./retroshare-0.5
 # ./clean.sh
 # ./makeSourcePackage.sh
 
-for dist in lucid; do
-	for arch in amd64; do
-		sudo PBUILDFOLDER=/var/cache/pbuilder pbuilder-dist "$dist" build *.dsc
-		cp /var/cache/pbuilder/"$dist"_result/retroshare_0.5-1_"$arch".deb ./RetroShare_0.5.1."$svn"_"$dist"_"$arch".deb
-	done
+for dist in lucid karmic; do
+		sudo PBUILDFOLDER=/var/cache/pbuilder pbuilder-dist "$dist" amd64 build *.dsc
+		cp /var/cache/pbuilder/"$dist"_result/retroshare_0.5-1_amd64.deb ./RetroShare_0.5.1."$svn"_"$dist"_amd64.deb
+		sudo PBUILDFOLDER=/var/cache/pbuilder pbuilder-dist "$dist" i386 build *.dsc
+		cp /var/cache/pbuilder/"$dist"-i386_result/retroshare_0.5-1_i386.deb ./RetroShare_0.5.1."$svn"_"$dist"_i386.deb
 done
 
--- build_scripts/Ubuntu_src/makeSourcePackage.sh
+++ build_scripts/Ubuntu_src/makeSourcePackage.sh
@@ -22,7 +22,7 @@ read tmp
 
 packages="."
 
-echo SVN number is $svnnumber
+echo SVN number is $svn
 echo version is $version
 
 echo Extracting base archive...
@@ -52,11 +52,11 @@ echo Setting version numbers...
 
 # setup version numbers
 cat retroshare-0.5/src/libretroshare/util/rsversion.h | grep -v SVN_REVISION > /tmp/toto2342
-echo \#define SVN_REVISION \"Revision: "$svnnumber"  date : `date`\" >> /tmp/toto2342
+echo \#define SVN_REVISION \"Revision: "$version"  date : `date`\" >> /tmp/toto2342
 cp /tmp/toto2342 retroshare-0.5/src/libretroshare/util/rsversion.h
 
 cat retroshare-0.5/src/retroshare-gui/util/rsversion.h | grep -v GUI_REVISION > /tmp/toto4463
-echo \#define GUI_REVISION \"Revision: "$svnnumber"  date : `date`\" >> /tmp/toto4463
+echo \#define GUI_REVISION \"Revision: "$version"  date : `date`\" >> /tmp/toto4463
 cp /tmp/toto4463 retroshare-0.5/src/retroshare-gui/util/rsversion.h
 
 # Various cleaning
