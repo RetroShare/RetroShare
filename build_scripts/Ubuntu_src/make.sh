d3deeecd8809c33013765ec7eca1e14a86006946
csoler <retroshare.project@gmail.com>
2011-02-22 20:58:05 +0000

    updated source package scripts
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/trunk@4049 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- build_scripts/Ubuntu_src/make.sh
+++ build_scripts/Ubuntu_src/make.sh
@@ -14,12 +14,11 @@ echo Revision number is $svn.
 echo Type ^C to abort, or enter to continue
 read tmp
 
-# rm -rf ./retroshare-0.5
-# ./clean.sh
+rm -rf ./retroshare-0.5
 # ./makeSourcePackage.sh
 
-for dist in lucid karmic; do
-		sudo PBUILDFOLDER=/var/cache/pbuilder pbuilder-dist "$dist" amd64 build *.dsc
+for dist in jaunty karmic lucid maverick; do
+		sudo PBUILDFOLDER=/var/cache/pbuilder pbuilder-dist "$dist" build *.dsc
 		cp /var/cache/pbuilder/"$dist"_result/retroshare_0.5-1_amd64.deb ./RetroShare_0.5.1."$svn"_"$dist"_amd64.deb
 		sudo PBUILDFOLDER=/var/cache/pbuilder pbuilder-dist "$dist" i386 build *.dsc
 		cp /var/cache/pbuilder/"$dist"-i386_result/retroshare_0.5-1_i386.deb ./RetroShare_0.5.1."$svn"_"$dist"_i386.deb
