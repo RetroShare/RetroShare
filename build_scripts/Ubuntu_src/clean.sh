8e4c7dbfc9eca5f5699381d38a108f675d552d30
csoler <retroshare.project@gmail.com>
2011-03-07 11:54:04 +0000

    fixed small issues in ubuntu packaging scripts
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/trunk@4084 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- build_scripts/Ubuntu_src/clean.sh
+++ build_scripts/Ubuntu_src/clean.sh
@@ -2,7 +2,7 @@
 
 rm ./retroshare_0.5.1-0.*_source.build
 rm ./retroshare_0.5.1-0.*_source.changes 
-rm ./retroshare_0.5.1.orig.tar.gz
+rm ./retroshare_0.5.1-0.*.tar.gz
 rm ./retroshare_0.5.1-0.*.diff.gz
 rm ./retroshare_0.5.1-0.*.dsc 
 
