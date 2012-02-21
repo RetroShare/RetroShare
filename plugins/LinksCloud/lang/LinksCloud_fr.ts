44d94d6c39c89817156cec07d47ee95187d29149
csoler <retroshare.project@gmail.com>
2012-02-21 15:47:47 +0000

    added french translation for linkscloud plugin
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/trunk@4977 b45a01b8-16f6-495d-af2f-9b41ad6348cc

---
+++ plugins/LinksCloud/lang/LinksCloud_fr.ts
@@ -0,0 +1,338 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>AddLinksDialog</name>
+    <message>
+        <location filename="../AddLinksDialog.ui" line="+14"/>
+        <location line="+81"/>
+        <source>Add Link</source>
+        <translation>Ajouter un lien</translation>
+    </message>
+    <message>
+        <location line="-30"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8.25pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;DejaVu Sans&apos;; font-size:18pt; font-weight:600; color:#ffffff;&quot;&gt;Add Link to Cloud&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished">&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8.25pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;DejaVu Sans&apos;; font-size:18pt; font-weight:600; color:#ffffff;&quot;&gt;Ajouter un lien dans le Cloud&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location line="+23"/>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <location line="+33"/>
+        <source>Add a new Link</source>
+        <translation>Ajouter un nouveau lien</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Title:</source>
+        <translation>Titre :</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Url:</source>
+        <translation>Url :</translation>
+    </message>
+    <message>
+        <location line="+50"/>
+        <source>Add Anonymous Link</source>
+        <translation>Ajouter un lien anonyme</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>+2 Great!</source>
+        <translation>+2 Parfait !</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>+1 Good</source>
+        <translation>+1 Bien</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>0 Okay</source>
+        <translation>0 Ok</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>-1 Sux</source>
+        <translation>-1 Bof</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>-2 Bad Link</source>
+        <translation>-2 Mauvais lien</translation>
+    </message>
+    <message>
+        <location filename="../AddLinksDialog.cpp" line="+58"/>
+        <source>New Link</source>
+        <translation>Nouveau lien</translation>
+    </message>
+    <message>
+        <location line="+28"/>
+        <source>Add Link Failure</source>
+        <translation type="unfinished">Ajouter défaillance du lien</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Missing Link and/or Title</source>
+        <translation>Lien et/ou tiitre manquant</translation>
+    </message>
+</context>
+<context>
+    <name>LinksCloudPlugin</name>
+    <message>
+        <location filename="../LinksCloudPlugin.cpp" line="+81"/>
+        <source>This plugin provides a set of cached links, and a voting system to promote them.</source>
+        <translation type="unfinished">Cette extension fournit un ensemble de liens en cache, et un système de vote pour les promouvoir.</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>LinksCloud</source>
+        <translation type="unfinished">LinksCloud</translation>
+    </message>
+</context>
+<context>
+    <name>LinksDialog</name>
+    <message>
+        <location filename="../LinksDialog.ui" line="+48"/>
+        <source>Title / Comment</source>
+        <translation>Titre / Commentaire</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Score</source>
+        <translation>Score</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Peer / Link</source>
+        <translation type="unfinished">Lien / Contact</translation>
+    </message>
+    <message>
+        <location line="+38"/>
+        <source>Sort by</source>
+        <translation>Trier par</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>Combo</source>
+        <translation>Combiné</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Time</source>
+        <translation>Temps</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Ranking</source>
+        <translation>Classement</translation>
+    </message>
+    <message>
+        <location line="+31"/>
+        <source>In last</source>
+        <translation>Par</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>Month</source>
+        <translation>Mois</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Week</source>
+        <translation>Semaine</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Day</source>
+        <translation>Jour</translation>
+    </message>
+    <message>
+        <location line="+31"/>
+        <source>From</source>
+        <translation>De</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>All Peers</source>
+        <translation>Tous les contacts</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Own Links</source>
+        <translation>Propre liens</translation>
+    </message>
+    <message>
+        <location line="+31"/>
+        <source>Show</source>
+        <translation>Montrer</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>Top 100</source>
+        <translation>Top 100</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>101-200</source>
+        <translation>101-200</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>201-300</source>
+        <translation>201-300</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>301-400</source>
+        <translation>301-400</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>401-500</source>
+        <translation>401-500</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Bottom 100</source>
+        <translation>100 derniers</translation>
+    </message>
+    <message>
+        <location line="+35"/>
+        <source>Link:</source>
+        <translation>Lien :</translation>
+    </message>
+    <message>
+        <location line="+32"/>
+        <source>Add Anonymous Link</source>
+        <translation>Ajouter un lien anonyme</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Add Link/Comment</source>
+        <translation>Ajouter Lien/Commentaire</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>Title:</source>
+        <translation>Titre :</translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Score:</source>
+        <translation>Score :</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>+2 Great!</source>
+        <translation>+2 Parfait !</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>+1 Good</source>
+        <translation>+1 Bien</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>0 Okay</source>
+        <translation>0 Ok</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>-1 Sux</source>
+        <translation>-1 Bof</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>-2 Bad Link</source>
+        <translation>-2 Mauvais lien</translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Url:</source>
+        <translation>Url :</translation>
+    </message>
+    <message>
+        <location line="+87"/>
+        <source>&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8.25pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Links Cloud&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished">&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8.25pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Links Cloud&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location line="+28"/>
+        <source>Add new link</source>
+        <translation>Ajouter un nouveau lien</translation>
+    </message>
+    <message>
+        <location filename="../LinksDialog.cpp" line="+139"/>
+        <source>Share Link Anonymously</source>
+        <translation>Partager le lien anonymement</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Vote on Link</source>
+        <translation type="unfinished">Vote sur le lien</translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>Download</source>
+        <translation type="unfinished">Télécharger</translation>
+    </message>
+    <message>
+        <location line="+655"/>
+        <source>Expand</source>
+        <translation>Montrer</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Hide</source>
+        <translation>Cacher</translation>
+    </message>
+    <message>
+        <location line="+195"/>
+        <source>File Request Confirmation</source>
+        <translation type="unfinished">Confirmation de demande de fichier</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>The file has been added to your download list.</source>
+        <translation>Le fichier a été ajouté à votre liste de téléchargement.</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>File Request canceled</source>
+        <translation>Demande de fichier annulée</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>The file has not been added to your download list, because you already have it.</source>
+        <translation type="unfinished">Le fichier n&apos;a pas été ajouté à votre liste de téléchargement, parce que vous l&apos;avez déjà.</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>File Request Error</source>
+        <translation type="unfinished">Erreur de demande de fichier</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>The file link is malformed.</source>
+        <translation type="unfinished">Le lien du fichier est incorrecte.</translation>
+    </message>
+</context>
+</TS>
