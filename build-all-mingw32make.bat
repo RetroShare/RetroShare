cd3ec84d153509fac9155884315ea3a9cc24ec52
defnax <retroshare.project@gmail.com>
2012-07-22 12:03:11 +0000

    improved build bat file (san)
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/trunk@5320 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- build-all-mingw32make.bat
+++ build-all-mingw32make.bat
@@ -3,47 +3,83 @@ set MINGW=%QTDIR%\mingw
 
 set PATH=%QTDIR%\qt\bin;%QTDIR%\bin;%MINGW%\bin;%PATH%
 
-"D:\Program Files\Tortoisesvn\bin\SubWCRev" . libretroshare\src\util\rsversion.in libretroshare\src\util\rsversion.h
+"C:\Program Files\TortoiseSVN\bin\SubWCRev" . libretroshare\src\util\rsversion.in libretroshare\src\util\rsversion.h
+
+@echo off
+:loop1
+if %1x == x (
+    rem if not exist tmp.txt echo debug >>tmp.txt
+    goto :end1
+)
+if /i %1==clean (
+    set clean=clean
+	shift
+    goto :loop1 
+)
+echo.%1>>tmp.txt
+shift
+goto :loop1
+
+:end1
+if not exist tmp.txt (
+    echo debug >>tmp.txt
+	set clean=clean
+)
+if %clean%x==cleanx (
+    if not exist tmp.txt echo %clean% >>tmp.txt
+    
+)
+for /f %%a in (tmp.txt) do (
+@echo on
 
 cd libbitdht\src
 
-mingw32-make clean 
+if not %clean%x==x mingw32-make clean 
 
 qmake libbitdht.pro
 
-mingw32-make
+mingw32-make %%a
 
 cd ..\..\libretroshare\src
 
-mingw32-make clean 
+if not %clean%x==x mingw32-make clean 
 
 qmake libretroshare.pro
 
-mingw32-make
+mingw32-make %%a
 
 cd ..\..\openpgpsdk\src
 
-mingw32-make clean 
+if not %clean%x==x mingw32-make clean 
 
 qmake openpgpsdk.pro
 
-mingw32-make
+mingw32-make %%a
 
 cd ..\..\retroshare-nogui\src
 
-mingw32-make clean 
+if not %clean%x==x mingw32-make clean 
 
 qmake retroshare-nogui.pro
 
-mingw32-make
+mingw32-make %%a
 
 cd ..\..\retroshare-gui\src
 
-mingw32-make clean 
+if not %clean%x==x mingw32-make clean
 
 qmake RetroShare.pro
 
-mingw32-make
+mingw32-make %%a
+
+cd ..\..
+
+)
+
+@echo off
+rem clean up
+set clean=
+del tmp.txt
 
 pause
 
