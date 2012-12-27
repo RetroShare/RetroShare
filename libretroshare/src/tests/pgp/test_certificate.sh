c11c60256886c2778d1fef5c08d6a784a511f6d0
csoler <retroshare.project@gmail.com>
2012-12-27 18:09:07 +0000

    fixed auto-test results
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/trunk@6049 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- libretroshare/src/tests/pgp/test_certificate.sh
+++ libretroshare/src/tests/pgp/test_certificate.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-./test_certificate -i test_certificate.oldformat.rsc
-./test_certificate -i test_certificate.newformat.rsc
+./test_certificate -i test_certificate.oldformat.rsc  && ./test_certificate -i test_certificate.newformat.rsc
