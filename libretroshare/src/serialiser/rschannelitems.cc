ace83e9ed5598b4b3e490e21561313f5cc496d63
csoler <retroshare.project@gmail.com>
2013-03-19 20:35:00 +0000

    removed debug info
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/trunk@6249 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- libretroshare/src/serialiser/rschannelitems.cc
+++ libretroshare/src/serialiser/rschannelitems.cc
@@ -29,7 +29,7 @@
 #include "serialiser/rsbaseserial.h"
 #include "serialiser/rstlvbase.h"
 
-#define RSSERIAL_DEBUG 0
+//#define RSSERIAL_DEBUG 0
 #include <iostream>
 
 /*************************************************************************/
