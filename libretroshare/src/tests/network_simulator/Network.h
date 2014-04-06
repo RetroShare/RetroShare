44a1f6f6f39ca41c31e30e05d8712aaad288a9a4
csoler <retroshare.project@gmail.com>
2014-04-06 07:56:20 +0000

    a bit of code splitting
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/trunk@7243 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- libretroshare/src/tests/network_simulator/Network.h
+++ libretroshare/src/tests/network_simulator/Network.h
@@ -6,55 +6,7 @@
 #include <list>
 #include <vector>
 #include <stdint.h>
-#include <turtle/p3turtle.h>
-
-class MonitoredTurtleRouter ;
-class MonitoredTurtleClient ;
-class RsTurtle ;
-class RsRawItem ;
-class p3ServiceServer ;
-
-class PeerNode
-{
-	public:
-		struct NodeTrafficInfo
-		{
-			std::map<std::string,std::string> local_src ;	// peer id., tunnel id
-			std::map<std::string,std::string> local_dst ;
-		};
-
-		PeerNode(const RsPeerId& id,const std::list<RsPeerId>& friends) ;
-		~PeerNode() ;
-
-		RsRawItem *outgoing() ;
-		void incoming(RsRawItem *) ;
-
-		const RsPeerId& id() const { return _id ;}
-
-		void tick() ;
-
-		const RsTurtle *turtle_service() const ;
-
-		// Turtle-related methods
-		//
-		void manageFileHash(const RsFileHash& hash) ;
-		void provideFileHash(const RsFileHash& hash) ;
-
-		const std::set<TurtleFileHash>& providedHashes() const { return _provided_hashes; }
-		const std::set<TurtleFileHash>& managedHashes() const { return _managed_hashes; }
-
-		void getTrafficInfo(NodeTrafficInfo& trinfo) ;	// 
-
-	private:
-		p3ServiceServer *_service_server ;
-        p3turtle *_turtle ;
-        MonitoredTurtleClient *_ftserver ;
-		pqiPublisher *_publisher ;
-		RsPeerId _id ;
-
-		std::set<TurtleFileHash> _provided_hashes ;
-		std::set<TurtleFileHash> _managed_hashes ;
-};
+#include "PeerNode.h"
 
 template<class NODE_TYPE> class Graph
 {
--- libretroshare/src/tests/network_simulator/network.pro
+++ libretroshare/src/tests/network_simulator/network.pro
@@ -7,11 +7,11 @@ INCLUDEPATH *= ../.. ..
 
 TARGET = NetworkSim
 
-SOURCES = Network.cpp  main.cpp NetworkViewer.cpp NetworkSimulatorGUI.cpp \
+SOURCES = Network.cpp  PeerNode.cpp main.cpp NetworkViewer.cpp NetworkSimulatorGUI.cpp \
           TurtleRouterStatistics.cpp RsAutoUpdatePage.cpp MonitoredRsPeers.cpp \
 			 MonitoredTurtle.cpp
 
-HEADERS = Network.h   MonitoredTurtle.h NetworkViewer.h NetworkSimulatorGUI.h \
+HEADERS = Network.h PeerNode.h   MonitoredTurtle.h NetworkViewer.h NetworkSimulatorGUI.h \
           TurtleRouterStatistics.h RsAutoUpdatePage.h MonitoredRsPeers.h
 
 FORMS = NetworkSimulatorGUI.ui TurtleRouterStatistics.ui
