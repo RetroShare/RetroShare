a270dec5c94a4b8a92087daa01d2eedcacd0289b
csoler <retroshare.project@gmail.com>
2014-12-16 19:20:53 +0000

    fixed utf8 in forum names
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/trunk@7763 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- retroshare-gui/src/gui/gxsforums/GxsForumThreadWidget.cpp
+++ retroshare-gui/src/gui/gxsforums/GxsForumThreadWidget.cpp
@@ -694,9 +694,9 @@ void GxsForumThreadWidget::insertGroupData(const RsGxsForumGroup &group)
     ui->forumName->setText(QString::fromUtf8(group.mMeta.mGroupName.c_str()));
 
     mForumDescription = QString();
-    mForumDescription += QString("<b>Forum name: \t</b>")+QString::fromStdString( group.mMeta.mGroupName)+"<br/>" ;
+    mForumDescription += QString("<b>Forum name: \t</b>")+QString::fromUtf8( group.mMeta.mGroupName.c_str())+"<br/>" ;
     mForumDescription += QString("<b>Subscribers: \t</b>")+QString::number( group.mMeta.mPop)+"<br/>" ;
-    mForumDescription += QString("<b>Posts: \t</b>")+QString::number(group.mMeta.mVisibleMsgCount)+"<br/>" ;
+    mForumDescription += QString("<b>Posts (at neighbor nodes): \t</b>")+QString::number(group.mMeta.mVisibleMsgCount)+"<br/>" ;
     mForumDescription += QString("<b>Description: </b>")+"<br/><br/>" ;
 
     mForumDescription += QString::fromUtf8(group.mDescription.c_str());
