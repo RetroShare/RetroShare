8f9e212e14f7664ffa0f3ee38f978e34513ca9cb
thunder2 <retroshare.project@gmail.com>
2015-01-07 12:46:29 +0000

    Rolled back changes on splitter stretch factor in IdDialog
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/trunk@7824 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- retroshare-gui/src/gui/Identity/IdDialog.cpp
+++ retroshare-gui/src/gui/Identity/IdDialog.cpp
@@ -176,10 +176,6 @@ IdDialog::IdDialog(QWidget *parent) :
 	/* Setup tree */
 	ui->treeWidget_IdList->sortByColumn(RSID_COL_NICKNAME, Qt::AscendingOrder);
 	ui->treeWidget_IdList->setContextMenuPolicy(Qt::CustomContextMenu) ;
-	
-	// Set initial size of the splitter
-  ui->splitter->setStretchFactor(0, 0);
-  ui->splitter->setStretchFactor(1, 1);
 
 	mIdQueue = new TokenQueue(rsIdentity->getTokenService(), this);
 
