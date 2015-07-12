f36b12c90f828bb5c14f91d743363437efaf26d4
thunder2 <retroshare.project@gmail.com>
2015-07-12 21:51:17 +0000

    Added missing restore of ostream format (std::dec) in p3ServiceServer::sendItem.
    git-svn-id: http://svn.code.sf.net/p/retroshare/code/trunk@8604 b45a01b8-16f6-495d-af2f-9b41ad6348cc

--- libretroshare/src/pqi/pqiservice.cc
+++ libretroshare/src/pqi/pqiservice.cc
@@ -183,7 +183,7 @@ bool p3ServiceServer::sendItem(RsRawItem *item)
 	// Packet Filtering.
 	if (!mServiceControl->checkFilter(item->PacketId() & 0xffffff00, item->PeerId()))
 	{
-		std::cerr << "p3ServiceServer::sendItem() Fails Filtering for packet id=" << std::hex << item->PacketId() << ", and peer " << item->PeerId() << std::endl;
+		std::cerr << "p3ServiceServer::sendItem() Fails Filtering for packet id=" << std::hex << item->PacketId() << std::dec << ", and peer " << item->PeerId() << std::endl;
 		delete item;
 		return false;
 	}
