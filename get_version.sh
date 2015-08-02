419da7e57d62fe658e6e5afdc03c3794bb8c43d6
Chozabu <chozabu@gmail.com>
2015-08-02 18:25:40 +0100

    update to new versioning method on linux/mac/bash - windows posh update needed

--- get_version.sh
+++ get_version.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-#note - though a single number is currently rather misleading
-#a version numer for approx human reading is nice
-version=$(($(git rev-list --count HEAD)+8613-2391))
-#+8613-2391 to approx matchching SVN number on caves conversion
+version=0x$(git log --pretty="%H" | head -1 | cut -c1-8)
 
 githash=$(git log --format="%H" -n 1)
 gitinfo=$(git describe --tags)
 gitbranch=$(git rev-parse --abbrev-ref HEAD)
+
+revcount=$(git rev-list --count HEAD)
