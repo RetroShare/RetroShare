34dec59728ee64604542972540797ce4df4b596c
Heini <heini@uncensored.citadel.org>
2015-09-06 20:07:39 +0200

    Create source package w/o re-cloning the repository (like the Redhat/Fedora script).
    The source package is now created from the currently checked out revision.

--- build_scripts/Debian+Ubuntu/makeSourcePackage.sh
+++ build_scripts/Debian+Ubuntu/makeSourcePackage.sh
@@ -1,43 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
 ###################### PARAMETERS ####################
-version="0.6.0"
-gitpath="https://github.com/RetroShare/RetroShare.git"
-workdir=retroshare06-${version}
-#bubba3="Y"		# comment out to compile for bubba3
+VERSION="0.6.0"
 ######################################################
+dirs -c
 
-echo This script is going to build the debian source package for RetroShare, from the Git repository.
-
-if test -d "${workdir}" ;  then
-    echo Removing the ${workdir} directory...
-    rm -rf ${workdir}
-fi
+echo "This script is going to build the debian source package for RetroShare, from a clone of the GitHub repository."
+echo
 
 # Parse options
-rev=""
-dist=""
+REV=""
+DISTS=""
 # This is the key for "Cyril Soler <csoler@sourceforge.net>"
-gpgkey="0932399B"
+GPGKEY="0932399B"
 
 while [ ${#} -gt 0 ]; do
     case ${1} in
-        "-rev") shift
-            rev=${1}
-            shift
-            ;;
         "-distribution") shift
-            dist=${1}
+            DISTS=${1}
             shift
             ;;
         "-key") shift
-            gpgkey=${1}
+            GPGKEY=${1}
             shift
             ;;
         "-h") shift
-            echo Package building script for debian/ubuntu distributions
-            echo Usage:
-            echo "  "${0} '-key [PGP key id] -rev [svn revision number]  -distribution [distrib name list with quotes, in (wheezy, sid, precise, saucy, etc)]'
+            echo "Package building script for debian/ubuntu distributions"
+            echo "Usage:"
+            echo "  ${0} '-key [PGP key id] -rev [svn revision number]  -distribution [distrib name list with quotes, in (wheezy, sid, precise, saucy, etc)]'"
             exit 1
             ;;
         "*") echo "Unknown option"
@@ -46,99 +36,83 @@ while [ ${#} -gt 0 ]; do
     esac
 done
 
-if test "${dist}" = "" ; then
-	dist="precise trusty vivid"
+if test "${DISTS}" = "" ; then
+	DISTS="precise trusty vivid"
 fi
 
 echo Attempting to get revision number...
-ccount=`git rev-list --count --all`
-ccount=`expr $ccount + 8613 - 8267`
+CCOUNT=`git rev-list --count --all`
+CCOUNT=`expr ${CCOUNT} + 8613 - 8267`
 
-date=`git log --pretty=format:"%ai" | head -1 | cut -d\  -f1 | sed -e s/-//g`
-time=`git log --pretty=format:"%aD" | head -1 | cut -d\  -f5 | sed -e s/://g`
-hhsh=`git log --pretty=format:"%H" | head -1 | cut -c1-8`
+DATE=`git log --pretty=format:"%ai" | head -1 | cut -d\  -f1 | sed -e s/-//g`
+TIME=`git log --pretty=format:"%aD" | head -1 | cut -d\  -f5 | sed -e s/://g`
+HASH=`git log --pretty=format:"%H" | head -1 | cut -c1-8`
 
-rev=${date}.${hhsh}
+GITROOT=`git rev-parse --show-toplevel`
+REV=${DATE}.${HASH}
+FULL_VERSION="${VERSION}"."${REV}"
 
-echo "  "Using PGP key id   : ${gpgkey}
-echo "  "Using distributions: ${dist}
-echo "  "Commit count       : ${ccount}
-echo "  "Date               : ${date}
-echo "  "Time               : ${time}
-echo "  "Hash               : ${hhsh}
-echo "  "Using revision     : ${rev}
-
-echo Done.
-version="${version}"."${rev}"
-echo Got version number ${version}. 
-echo Please check that the changelog is up to date. 
-echo Hit ENTER if this is correct. Otherwise hit Ctrl+C 
+echo "Using PGP key id    : ${GPGKEY}"
+echo "Using distributions : ${DISTS}"
+echo "Commit count        : ${CCOUNT}"
+echo "Date                : ${DATE}"
+echo "Time                : ${TIME}"
+echo "Hash                : ${HASH}"
+echo "Using revision      : ${REV}"
+echo "Using version number: ${VERSION}"
+echo
+echo "Hit ENTER if this is correct. Otherwise hit Ctrl+C"
 read tmp
 
-echo Extracting base archive...
+WORKDIR="retroshare06-${FULL_VERSION}"
 
-mkdir -p ${workdir}/src
-echo Checking out latest snapshot...
-cd ${workdir}/src
-git clone --depth 1 https://github.com/RetroShare/RetroShare.git .
-cd -
-
-if ! test -d ${workdir}/src/libretroshare/; then
-	echo Git clone failed. 
-	exit
+if [[ -d ${WORKDIR} ]]
+then
+    echo "Removing the directory ${WORKDIR}..."
+    rm -rf ${WORKDIR}
 fi
+mkdir -p ${WORKDIR}/src
 
-#cp -r data   ${workdir}/src/
-cp -r debian ${workdir}/debian
-
-#svn co -r${rev} ${svnpath}/trunk/ . 
+echo "Copying sources into workdir..."
+rsync -rc --exclude build_scripts ${GITROOT}/* ${WORKDIR}/src
+cp -r debian ${WORKDIR}/debian
+pushd ${WORKDIR} >/dev/null
 
 # VOIP tweak  
-cp ${workdir}/src/retroshare-gui/src/gui/chat/PopupChatDialog.ui ${workdir}/src/plugins/VOIP/gui/PopupChatDialog.ui
+cp src/retroshare-gui/src/gui/chat/PopupChatDialog.ui src/plugins/VOIP/gui/PopupChatDialog.ui
 
-#   # handling of libssh
-#   LIBSSH_VERSION=0.6.4
-#   LIBSSH_LOCATION=https://git.libssh.org/projects/libssh.git/snapshot/libssh-${LIBSSH_VERSION}.tar.gz
-#   
-#   [ -f libssh-${LIBSSH_VERSION}.tar.gz ] || wget --no-check-certificate -O libssh-${LIBSSH_VERSION}.tar.gz $LIBSSH_LOCATION 
-#   tar zxvf ../libssh-${LIBSSH_VERSION}.tar.gz
+echo "Setting version numbers..."
+sed -e "s%RS_REVISION_NUMBER.*%RS_REVISION_NUMBER   0x${HASH}%"  src/libretroshare/src/retroshare/rsversion.in > src/libretroshare/src/retroshare/rsversion.h
 
-# Cloning sqlcipher
-# git clone https://github.com/sqlcipher/sqlcipher.git
+echo "Calling debuild..."
+for DIST in ${DISTS}; do
+    echo "opying changelog for ${DIST}..."
+    sed -e s/XXXXXX/"${REV}"/g -e s/YYYYYY/"${DIST}"/g ../changelog > debian/changelog
 
-# cleaning up protobof generated files
-# rm -f src/retroshare-nogui/src/rpc/proto/gencc/*.pb.h
-# rm -f src/retroshare-nogui/src/rpc/proto/gencc/*.pb.cc
+    [ -d sqlcipher ] && rm -rf sqlcipher
 
-cd ${workdir}
-echo Setting version numbers...
+    if test "${DIST}" = "lucid" ; then
+        cp ../control.ubuntu_${DIST} debian/control
+    elif test "${DIST}" = "squeeze" ; then
+        cp ../control.${DIST}_bubba3 debian/control
+    elif test "${DIST}" = "precise" ; then
+        cp ../control.${DIST} debian/control
+    elif test "${DIST}" = "wheezy" -o "${DIST}" = "jessie" ; then
+        # Cloning sqlcipher
+        git clone https://github.com/sqlcipher/sqlcipher.git
+        pushd sqlcipher >/dev/null
+        git checkout v3.3.1
+        rm -rf .git
+        popd >/dev/null
 
-# setup version numbers
-sed -e "s%RS_REVISION_NUMBER.*%RS_REVISION_NUMBER   0x${hhsh}%"  src/libretroshare/src/retroshare/rsversion.in > src/libretroshare/src/retroshare/rsversion.h
-
-# Various cleaning
-echo Cleaning...
-
-\rm -rf src/.git
-#find . -depth -name ".svn" -a -type d -exec rm -rf {} \;    # remove all svn repositories
-
-echo Calling debuild...
-for i in ${dist}; do
-    echo copying changelog for ${i}
-    sed -e s/XXXXXX/"${rev}"/g -e s/YYYYYY/"${i}"/g ../changelog > debian/changelog
-
-    if test "${i}" = "lucid" ; then
-        cp ../control.ubuntu_lucid debian/control
-    elif test "${i}" = "squeeze" ; then
-        cp ../control.squeeze_bubba3 debian/control
-    elif test "${i}" = "precise" ; then
-        cp ../control.precise debian/control
+        cp ../control.${DIST} debian/control
+        cp ../rules.${DIST} debian/rules
     else
         cp ../debian/control debian/control
     fi
 
-    debuild -S -k${gpgkey}
+    debuild -S -k${GPGKEY}
 done
-cd -
+popd >/dev/null
 
 exit 0
