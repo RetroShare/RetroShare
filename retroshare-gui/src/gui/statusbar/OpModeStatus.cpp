1ff1b8960fa58a99c4ba63db718c00115ec3de19
Phenom <retrosharephenom@gmail.com>
2016-02-07 11:45:55 +0100

    Save last state of OpMode status bar droplist and restore it at start.
    Add colored style sheet to OpModeStatus ComboBox.
    And defined default color when stylesheet file are not found.

--- retroshare-gui/src/gui/statusbar/OpModeStatus.cpp
+++ retroshare-gui/src/gui/statusbar/OpModeStatus.cpp
@@ -15,7 +15,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor,
  *  Boston, MA  02110-1301, USA.
  ****************************************************************/
 
@@ -23,25 +23,39 @@
 #include <QLabel>
 
 #include "gui/statusbar/OpModeStatus.h"
+#include "gui/settings/rsharesettings.h"
+
 #include <retroshare/rsconfig.h>
+
 #include <iostream>
 
 OpModeStatus::OpModeStatus(QWidget *parent)
- : QComboBox(parent)
+	: QComboBox(parent)
 {
+	onUpdate = false;
+	opMode_Full_Color = QColor("#CCFFCC");
+	opMode_NoTurtle_Color = QColor("#CCCCFF");
+	opMode_Gaming_Color = QColor("#FFFFCC");
+	opMode_Minimal_Color = QColor("#FFCCCC");
+
 	/* add the options */
 	addItem(tr("Normal Mode"), RS_OPMODE_FULL);
+	setItemData(0, opMode_Full_Color, Qt::BackgroundRole);
 	addItem(tr("No Anon D/L"), RS_OPMODE_NOTURTLE);
+	setItemData(1, opMode_NoTurtle_Color, Qt::BackgroundRole);
 	addItem(tr("Gaming Mode"), RS_OPMODE_GAMING);
+	setItemData(2, opMode_Gaming_Color, Qt::BackgroundRole);
 	addItem(tr("Low Traffic"), RS_OPMODE_MINIMAL);
+	setItemData(3, opMode_Minimal_Color, Qt::BackgroundRole);
 
 	connect(this, SIGNAL(activated( int )), this, SLOT(setOpMode()));
 
+	setCurrentIndex(Settings->valueFromGroup("StatusBar", "OpMode", QVariant(0)).toInt());
+	setOpMode();
 	setToolTip(tr("Use this DropList to quickly change Retroshare's behaviour\n No Anon D/L: switches off file forwarding\n Gaming Mode: 25% standard traffic and TODO: reduced popups\n Low Traffic: 10% standard traffic and TODO: pauses all file-transfers"));
 
 	setFocusPolicy(Qt::ClickFocus);
 }
-
 
 void OpModeStatus::getOpMode()
 {
@@ -51,17 +65,26 @@ void OpModeStatus::getOpMode()
 		default:
 		case RS_OPMODE_FULL:
 			setCurrentIndex(0);
-			break;
+			setProperty("opMode", "Full");
+		break;
 		case RS_OPMODE_NOTURTLE:
 			setCurrentIndex(1);
-			break;
+			setProperty("opMode", "NoTurtle");
+		break;
 		case RS_OPMODE_GAMING:
 			setCurrentIndex(2);
-			break;
+			setProperty("opMode", "Gaming");
+		break;
 		case RS_OPMODE_MINIMAL:
 			setCurrentIndex(3);
-			break;
+			setProperty("opMode", "Minimal");
+		break;
 	}
+	onUpdate = true;
+	style()->unpolish(this);
+	style()->polish(this);
+	update();
+	onUpdate = false;
 }
 
 void OpModeStatus::setOpMode()
@@ -69,14 +92,65 @@ void OpModeStatus::setOpMode()
 	std::cerr << "OpModeStatus::setOpMode()";
 	std::cerr << std::endl;
 
-        int idx = currentIndex();
-        QVariant var = itemData(idx);
-        uint32_t opMode = var.toUInt();
+	int idx = currentIndex();
+	QVariant var = itemData(idx);
+	uint32_t opMode = var.toUInt();
 
 	rsConfig->setOperatingMode(opMode);
 
 	// reload to be safe.
 	getOpMode();
+	Settings->setValueToGroup("StatusBar", "OpMode", idx);
 }
 
+QColor OpModeStatus::getOpMode_Full_Color() const
+{
+	return opMode_Full_Color;
+}
 
+void OpModeStatus::setOpMode_Full_Color( QColor c )
+{
+	opMode_Full_Color = c;
+	setItemData(0, opMode_Full_Color, Qt::BackgroundRole);
+	if (!onUpdate)
+		getOpMode();
+}
+
+QColor OpModeStatus::getOpMode_NoTurtle_Color() const
+{
+	return opMode_NoTurtle_Color;
+}
+
+void OpModeStatus::setOpMode_NoTurtle_Color( QColor c )
+{
+	opMode_NoTurtle_Color = c;
+	setItemData(1, opMode_NoTurtle_Color, Qt::BackgroundRole);
+	if (!onUpdate)
+		getOpMode();
+}
+
+QColor OpModeStatus::getOpMode_Gaming_Color() const
+{
+	return opMode_Gaming_Color;
+}
+
+void OpModeStatus::setOpMode_Gaming_Color( QColor c )
+{
+	opMode_Gaming_Color = c;
+	setItemData(2, opMode_Gaming_Color, Qt::BackgroundRole);
+	if (!onUpdate)
+		getOpMode();
+}
+
+QColor OpModeStatus::getOpMode_Minimal_Color() const
+{
+	return opMode_Minimal_Color;
+}
+
+void OpModeStatus::setOpMode_Minimal_Color( QColor c )
+{
+	opMode_Minimal_Color = c;
+	setItemData(3, opMode_Minimal_Color, Qt::BackgroundRole);
+	if (!onUpdate)
+		getOpMode();
+}
