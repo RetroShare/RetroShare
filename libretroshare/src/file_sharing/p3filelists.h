a6d467d13851aaad75bd1773309b9be80ea47402
mr-alice <mralice@users.sourceforge.net>
2016-07-18 21:33:54 -0400

    added interface for p3FileLists

--- libretroshare/src/file_sharing/p3filelists.h
+++ libretroshare/src/file_sharing/p3filelists.h
@@ -19,11 +19,68 @@
 // The file lists are not directry updated. A FileListWatcher class is responsible for this
 // in every case. 
 //
-class p3FileLists: public p3Service
+#include <services/p3service.h>
+#include <pqi/p3cfgmgr.h>
+
+class p3FileLists: public p3Service, public p3Config, public RsSharedFileService 
 {
 	public:
-		p3FileLists() ;
+		typedef uint64_t EntryIndex ;	// this should probably be defined elsewhere
 
+		p3FileLists(mPeerMgr *mpeers) ;
 
+		/*   
+		 */
+		 
+		// derived from p3Service
+		//
+		virtual int tick() ;
+
+		// access to own/remote shared files
+		//
+		virtual bool findLocalFile(const RsFileHash& hash,FileSearchFlags flags,const RsPeerId& peer_id, std::string &fullpath, uint64_t &size,FileStorageFlags& storage_flags,std::list<std::string>& parent_groups) const;
+
+		virtual int SearchKeywords(const std::list<std::string>& keywords, std::list<DirDetails>& results,FileSearchFlags flags,const RsPeerId& peer_id) ;
+		virtual int SearchBoolExp(Expression *exp, std::list<DirDetails>& results,FileSearchFlags flags,const RsPeerId& peer_id) const ;
+
+		// Interface for browsing dir hierarchy
+		//
+		int RequestDirDetails(EntryIndex, DirDetails&, FileSearchFlags) const ;
+		uint32_t getType(const EntryIndex&) const ;
+		int RequestDirDetails(const std::string& path, DirDetails &details) const ;
+
+      // set/update shared directories
+		void setSharedDirectories(const std::list<SharedDirInfo>& dirs);
+		void getSharedDirectories(std::list<SharedDirInfo>& dirs);
+		void updateShareFlags(const SharedDirInfo& info) ;
+
+		void forceDirectoryCheck();              // Force re-sweep the directories and see what's changed
+		bool inDirectoryCheck();
+
+		// Derived from p3Config
+		//
+	private:
+		p3PeerMgr *mPeers ;
+
+		// File sync request queues. The fast one is used for online browsing when friends are connected.
+		// The slow one is used for background update of file lists.
+		//
+		std::list<RsFileListSyncRequest> mFastRequestQueue ;
+		std::list<RsFileListSyncRequest> mSlowRequestQueue ;
+
+		// Directory storage hierarchies
+		//
+		std::map<RsPeerId,RemoteDirectoryStorage *> mRemoteDirectories ;
+		
+		LocalSharedDirectoryMap *mLocalSharedDirs ;
+
+		RemoteSharedDirectoryWatcher *mRemoteDirWatcher ;
+		LocalSharedDirectoryWatcher *mLocalDirWatcher ;
+
+		// We use a shared file cache as well, to avoid re-hashing files with known modification TS and equal name.
+		//
+		HashCache *mHashCache ;
+
+		RsMutex mFLSMtx ;
 };
 
