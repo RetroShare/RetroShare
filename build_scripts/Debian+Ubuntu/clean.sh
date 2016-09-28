c2761659e9afa502d80d24792e06fe25525dd85f
csoler <csoler@users.sourceforge.net>
2016-09-28 22:28:43 +0200

    updated clean.sh script to 0.6.1

--- build_scripts/Debian+Ubuntu/clean.sh
+++ build_scripts/Debian+Ubuntu/clean.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 rm -f ./libssh-0.5.4.tar.gz.*
-rm -f ./retroshare06_0.6.0-1.*_source.build
-rm -f ./retroshare06_0.6.0-1.*_source.changes 
-rm -f ./retroshare06_0.6.0-1.*.tar.gz
-rm -f ./retroshare06_0.6.0-1.*.diff.gz
-rm -f ./retroshare06_0.6.0-1.*.dsc 
+rm -f ./retroshare06_0.6.1-1.*_source.build
+rm -f ./retroshare06_0.6.1-1.*_source.changes 
+rm -f ./retroshare06_0.6.1-1.*.tar.gz
+rm -f ./retroshare06_0.6.1-1.*.diff.gz
+rm -f ./retroshare06_0.6.1-1.*.dsc 
 rm -f *.upload
 
 rm -f *~
