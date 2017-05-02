fcca44d1e6da29311295ef782240f3fb7c1673df
Gioacchino Mazzurco <gio@eigenlab.org>
2017-05-02 12:20:27 +0200

    Android qml app play notification sound

--- retroshare-qml-app/src/android/src/org/retroshare/android/qml_app/RetroShareAndroidNotifyService.java
+++ retroshare-qml-app/src/android/src/org/retroshare/android/qml_app/RetroShareAndroidNotifyService.java
@@ -23,6 +23,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
 import android.net.Uri;
 
 import org.qtproject.qt5.android.bindings.QtService;
@@ -36,7 +37,8 @@ public class RetroShareAndroidNotifyService extends QtService
 		mBuilder.setSmallIcon(R.drawable.retroshare06_48x48)
 				.setContentTitle(title)
 				.setContentText(text)
-				.setAutoCancel(true);
+				.setAutoCancel(true)
+				.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
 
 		Intent intent = new Intent(this, RetroShareQmlActivity.class);
 		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
