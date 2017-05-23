483d5386ba8a02f3bde3198f5dd9b65b69b978b0
defnax <defnator@gmail.com>
2017-05-23 18:46:50 +0200

    fit the background image to the login page

--- retroshare-gui/src/mingw32make.bat
+++ retroshare-gui/src/mingw32make.bat
@@ -1,11 +1,13 @@
-set QTDIR=C:\Qt\4.8.6
-set MINGW=C:\MinGW
+set QTDIR=C:\Qt\5.5.0\5.5\mingw492_32
+set MINGW=C:\Qt\Tools\mingw492_32
+
 set GIT=C:\Program Files\Git
 
 set PATH=%QTDIR%\bin;%MINGW%\bin;%GIT%\bin;%PATH%
 
 
 qmake retroshare-gui.pro "CONFIG+=version_detail_bash_script" 
+
 
 mingw32-make
 
