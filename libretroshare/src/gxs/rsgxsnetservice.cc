d83a9625cc4d228f77a7db300e2d773d43a88b32
Phenom <retrosharephenom@gmail.com>
2017-07-15 16:45:19 +0200

    Fix clang warning: unused variables 'GIXS_CUT_OFF', ...
    /libretroshare/src/gxs/rsgxsnetservice.cc:247: warning: unused variable
    'GIXS_CUT_OFF' [-Wunused-const-variable]
    static const uint32_t GIXS_CUT_OFF                            =
    0;
    /libretroshare/src/gxs/rsgxsnetservice.cc:251: warning: unused variable
    'SECURITY_DELAY_TO_FORCE_CLIENT_REUPDATE' [-Wunused-const-variable]
    static const uint32_t SECURITY_DELAY_TO_FORCE_CLIENT_REUPDATE =
    3600; // force re-update if there happens to be a large delay between
    our server side TS and the client side TS of friends

--- libretroshare/src/gxs/rsgxsnetservice.cc
+++ libretroshare/src/gxs/rsgxsnetservice.cc
@@ -244,11 +244,13 @@
 // A small value for MAX_REQLIST_SIZE is likely to help messages to propagate in a chaotic network, but will also slow them down.
 // A small SYNC_PERIOD fasten message propagation, but is likely to overload the server side of transactions (e.g. overload outqueues).
 //
-static const uint32_t GIXS_CUT_OFF                            =            0;
+//static const uint32_t GIXS_CUT_OFF                            =            0;
 static const uint32_t SYNC_PERIOD                             =           60;
 static const uint32_t MAX_REQLIST_SIZE                        =           20; // No more than 20 items per msg request list => creates smaller transactions that are less likely to be cancelled.
 static const uint32_t TRANSAC_TIMEOUT                         =         2000; // In seconds. Has been increased to avoid epidemic transaction cancelling due to overloaded outqueues.
+#ifdef TO_REMOVE
 static const uint32_t SECURITY_DELAY_TO_FORCE_CLIENT_REUPDATE =         3600; // force re-update if there happens to be a large delay between our server side TS and the client side TS of friends
+#endif
 static const uint32_t REJECTED_MESSAGE_RETRY_DELAY            =      24*3600; // re-try rejected messages every 24hrs. Most of the time this is because the peer's reputation has changed.
 static const uint32_t GROUP_STATS_UPDATE_DELAY                =          240; // update unsubscribed group statistics every 3 mins
 static const uint32_t GROUP_STATS_UPDATE_NB_PEERS             =            2; // number of peers to which the group stats are asked
