9c391cb01565684a9b32728778a605a2605d4dfe
csoler <csoler@users.sourceforge.net>
2017-07-15 22:15:51 +0200

    added an additional param to BinToHex so as to limit the size of the output

--- libretroshare/src/util/rsprint.h
+++ libretroshare/src/util/rsprint.h
@@ -36,7 +36,10 @@ namespace RsUtil {
 
 std::string BinToHex(const std::string &bin);
 std::string BinToHex(const char *arr, const uint32_t len);
-std::string BinToHex(const unsigned char *arr, const uint32_t len);
+
+// proxy function. When max_len>0 and len>max_len, only the first "max_len" bytes are writen to the string and "..." is happened.
+
+std::string BinToHex(const unsigned char *arr, const uint32_t len, uint32_t max_len=0);
 std::string NumberToString(uint64_t n, bool hex=false);
 std::string HashId(const std::string &id, bool reverse = false);
 std::vector<uint8_t> BinToSha256(const std::vector<uint8_t> &in);
