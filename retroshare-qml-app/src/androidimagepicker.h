c570aae9f742b1522d1eeeab515323228e3afaa6
Angela Mazzurco <giangiasoc17@gmail.com>
2017-07-18 16:16:35 +0200

    Function to convert an image to a png base64 format

--- retroshare-qml-app/src/androidimagepicker.h
+++ retroshare-qml-app/src/androidimagepicker.h
@@ -3,6 +3,12 @@
 #include <QObject>
 #include <QDebug>
 
+#include <QFile>
+#include <QUrl>
+#include <QImage>
+#include <QImageReader>
+#include <QBuffer>
+
 #ifdef __ANDROID__
 #	include <QtAndroid>
 #	include <QtAndroidExtras/QAndroidJniObject>
@@ -18,10 +24,38 @@ public slots:
 	{
 		qDebug() << "Starting image picker intent";
 
-        #ifdef __ANDROID__
-		    QtAndroid::androidActivity().callMethod<void>(
-			                            "openImagePicker",
-			                            "()V" );
-        #endif // __ANDROID__
+#ifdef __ANDROID__
+		QtAndroid::androidActivity().callMethod<void>(
+		                            "openImagePicker",
+		                            "()V" );
+#endif // __ANDROID__
+
+	}
+
+	// Used to convert a given image path into a png base64 string
+	static QString imageToBase64 (QString  const& path)
+	{
+		// Get local path from uri
+		QUrl url (path);
+		QString localPath = url.toLocalFile();
+
+		// Read the image
+		QImageReader reader;
+		reader.setFileName(localPath);
+		QImage image = reader.read();
+
+		// Transform image into PNG format
+		QByteArray ba;
+		QBuffer    buffer( &ba );
+		buffer.open( QIODevice::WriteOnly );
+		image.save( &buffer, "PNG" );
+
+		// Get Based 64 image string
+		QString encoded = QString(ba.toBase64());
+
+		qDebug() << "imageToBase64() " ;
+
+		return encoded;
+
 	}
 };
