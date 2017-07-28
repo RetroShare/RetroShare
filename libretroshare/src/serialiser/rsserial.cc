a5b7873c00f634cac36003166cbd9adb2c1087a2
Phenom <retrosharephenom@gmail.com>
2017-07-28 12:57:28 +0200

    Fix CppCheck in rsserial.cc
    /libretroshare/src/serialiser/rsserial.cc:541: warning:
    Cppcheck(passedByValue): Function parameter 'clsName' should be passed
    by reference.
    /libretroshare/src/serialiser/rsserial.cc:549: warning:
    Cppcheck(passedByValue): Function parameter 'clsName' should be passed
    by reference.
    /libretroshare/src/serialiser/rsserial.cc:67: warning:
    Cppcheck(noExplicitConstructor): Class 'Counter' has a constructor with
    1 argument that is not explicit.

--- libretroshare/src/serialiser/rsserial.cc
+++ libretroshare/src/serialiser/rsserial.cc
@@ -64,7 +64,7 @@ RsItem::RsItem(uint32_t t)
 class Counter
 {
 	public: 
-		Counter(int i): _i(i) {}
+		static Counter(int i): _i(i) {}
 		Counter(): _i(0) {} 
 
 		int v() const { return _i ; }
@@ -538,7 +538,7 @@ uint16_t  getRsItemService(uint32_t type)
 }
 
 
-std::ostream &printRsItemBase(std::ostream &out, std::string clsName, uint16_t indent)
+std::ostream &printRsItemBase(std::ostream &out, const std::string &clsName, uint16_t indent)
 {
         printIndent(out, indent);
 	out << "RsItem: " << clsName << " ####################################";
@@ -546,7 +546,7 @@ std::ostream &printRsItemBase(std::ostream &out, std::string clsName, uint16_t i
         return out;
 }
 
-std::ostream &printRsItemEnd(std::ostream &out, std::string clsName, uint16_t indent)
+std::ostream &printRsItemEnd(std::ostream &out, const std::string &clsName, uint16_t indent)
 {
         printIndent(out, indent);
         out << "###################### " << clsName << " #####################";
