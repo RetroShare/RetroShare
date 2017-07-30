2206e805275788ea78aa8b486df439b1439b01e8
Phenom <retrosharephenom@gmail.com>
2017-07-30 19:42:52 +0200

    Fix CppCheck in SpeexProcessor.h
    /plugins/VOIP/gui/SpeexProcessor.h:46: warning:
    Cppcheck(noExplicitConstructor): Class 'SpeexInputProcessor' has a
    constructor with 1 argument that is not explicit.
    /plugins/VOIP/gui/SpeexProcessor.h:92: warning:
    Cppcheck(noExplicitConstructor): Class 'SpeexOutputProcessor' has a
    constructor with 1 argument that is not explicit.

--- plugins/VOIP/gui/SpeexProcessor.h
+++ plugins/VOIP/gui/SpeexProcessor.h
@@ -43,7 +43,7 @@ namespace QtSpeex {
 	public:
                 float dPeakSpeaker, dPeakSignal, dMaxMic, dPeakMic, dPeakCleanMic, dVoiceAcivityLevel;
                 float fSpeechProb;
-                SpeexInputProcessor(QObject* parent = 0);
+                explicit SpeexInputProcessor(QObject* parent = 0);
                 virtual ~SpeexInputProcessor();
 
 		bool hasPendingPackets();
@@ -89,7 +89,7 @@ namespace QtSpeex {
                 Q_OBJECT
 
         public:
-                SpeexOutputProcessor(QObject* parent = 0);
+                explicit SpeexOutputProcessor(QObject* parent = 0);
                 virtual ~SpeexOutputProcessor();
 
                 void putNetworkPacket(QString name, QByteArray packet);
