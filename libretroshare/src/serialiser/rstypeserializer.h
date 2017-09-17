d89c151a4df2c86193a8af8a7830420ba6515f0d
Gioacchino Mazzurco <gio@eigenlab.org>
2017-09-17 14:41:18 +0200

    Fix typo

--- libretroshare/src/serialiser/rstypeserializer.h
+++ libretroshare/src/serialiser/rstypeserializer.h
@@ -45,7 +45,7 @@
 /** @def RS_REGISTER_SERIAL_MEMBER_TYPED(I, T)
  * This macro usage is similar to @see RS_REGISTER_SERIAL_MEMBER(I) but it
  * permit to force serialization/deserialization type, it is expecially useful
- * with enum class members ot RsTlvItem derivative members, be very careful with
+ * with enum class members or RsTlvItem derivative members, be very careful with
  * the type you pass, as reinterpret_cast on a reference is used that is
  * expecially permissive so you can shot your feet if not carefull enough.
  *
