9a54205addf43ea9488a46fae0475cc33c331b11
csoler <csoler@users.sourceforge.net>
2018-01-18 22:40:28 +0100

    added time.h include for windows compilation

--- retroshare-gui/src/TorControl/TorControl.cpp
+++ retroshare-gui/src/TorControl/TorControl.cpp
@@ -30,6 +30,8 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <time.h>
+
 #include "TorControl.h"
 #include "TorControlSocket.h"
 #include "HiddenService.h"
