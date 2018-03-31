RetroShare/RetroShare

commit add9c1e72c895a8774709ba676034ea59fbbdc23

chatwidget: do not set focus to edit when it is not necessary
e.g. when a user just wants to browse chat history

retroshare-gui/src/gui/chat/ChatWidget.cpp
@@ -731,15 +731,6 @@ bool ChatWidget::eventFilter(QObject *obj, QEvent *event)
 			}
 
 		}
-	} else {
-		if (event->type() == QEvent::WindowActivate) {
-			if (isVisible() && (window() == NULL || window()->isActiveWindow())) {
-				newMessages = false;
-				emit infoChanged(this);
-				focusDialog();
-                ChatUserNotify::clearWaitingChat(chatId);
-			}
-		}
 	}
 	// pass the event on to the parent class
 	return QWidget::eventFilter(obj, event);
@@ -889,7 +880,17 @@ void ChatWidget::showEvent(QShowEvent */*event*/)
 {
 	newMessages = false;
 	emit infoChanged(this);
-	focusDialog();
+	// if user waded through the jungle of history just let him on
+	// own decide whether to continue the journey or start typing
+	QScrollBar *scrollbar = ui->textBrowser->verticalScrollBar();
+	bool is_scrollbar_at_end = scrollbar->value() == scrollbar->maximum();
+	bool is_chat_text_edit_empty = ui->chatTextEdit->toPlainText().isEmpty();
+	if (is_scrollbar_at_end || !is_chat_text_edit_empty) {
+		focusDialog();
+	} else {
+		// otherwise focus will be get even not chat itself
+		ui->textBrowser->setFocus();
+	}
     ChatUserNotify::clearWaitingChat(chatId);
 
 	if (firstShow) {

commit c821c179ef74a0b1ffe3979a3c56e998410765a0

Fix private chat icon not changing back to idle icon from new message icon when window activated Broken in add9c1e72c895a8774709ba676034ea59fbbdc23

retroshare-gui/src/gui/chat/ChatWidget.cpp
@@ -743,6 +743,13 @@ bool ChatWidget::eventFilter(QObject *obj, QEvent *event)
 			}
 
 		}
+	} else {
+			if (event->type() == QEvent::WindowActivate) {
+				if (isVisible() && (window() == NULL || window()->isActiveWindow())) {
+					newMessages = false;
+					emit infoChanged(this);
+				}
+			}
 	}
 	// pass the event on to the parent class
 	return QWidget::eventFilter(obj, event);