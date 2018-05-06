5120adeee913ca633437554d7c2d611ec5277f4d
Phenom <retrosharephenom@gmail.com>
2018-05-06 16:57:02 +0200

    Fix CppCheck in pluginmanager.cc
    /libretroshare/src/plugins/pluginmanager.cc:560: warning:
    Cppcheck(uninitMemberVar): Member variable
    'RsPQIService::_tick_delay_in_seconds' is not initialized in the
    constructor.

--- libretroshare/src/plugins/pluginmanager.cc
+++ libretroshare/src/plugins/pluginmanager.cc
@@ -559,6 +559,7 @@ bool RsPluginManager::saveList(bool& cleanup, std::list<RsItem*>& list)
 
 RsPQIService::RsPQIService(uint16_t /*service_type*/, uint32_t /*tick_delay_in_seconds*/, RsPluginHandler* /*pgHandler*/)
 	: p3Service(),p3Config()
+	, _tick_delay_in_seconds(0)
 {
 }
 
