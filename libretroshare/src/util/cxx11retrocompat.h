3ad24f9c78c3672a3e0f227b37535e37edd164b7
UnseenP2P <43121470+unsenep2pdev@users.noreply.github.com>
2018-10-04 18:53:21 -0700

    Update cxx11retrocompat.h

--- libretroshare/src/util/cxx11retrocompat.h
+++ libretroshare/src/util/cxx11retrocompat.h
@@ -22,31 +22,19 @@
 #pragma once
 
 
-//#ifdef __GNUC__
-//#	define GCC_VERSION (__GNUC__*10000+__GNUC_MINOR__*100+__GNUC_PATCHLEVEL__)
-//#	if GCC_VERSION < 40700
-//#		define override
-//#		define final
-//#	endif // GCC version < 40700
-//#	if GCC_VERSION < 40600
-//#		define nullptr NULL
-//#	endif // GCC_VERSION < 40600
-//#endif //defined GNUC
-
-
-//#ifdef __GNUC__
-//#define GCC_VERSION (__GNUC__*10000+__GNUC_MINOR__*100+__GNUC_PATCHLEVEL__)
-//#if GCC_VERSION < 40700
-//#		define override
-//#		define final
-//#endif
-//#if __APPLE__
-//#if __cplusplus < 201103L
-//#define nullptr NULL
-//#endif
-//#else// GCC version < 40700
-//#if GCC_VERSION < 40600
-//#define nullptr NULL
-//#endif // GCC_VERSION < 40600
-//#endif //non mac OS(es)
-//#endif
+#ifdef __GNUC__
+#define GCC_VERSION (__GNUC__*10000+__GNUC_MINOR__*100+__GNUC_PATCHLEVEL__)
+#if GCC_VERSION < 40700
+#define override
+#define final
+#endif
+#if __APPLE__
+#if __cplusplus < 201103L
+#define nullptr NULL
+#endif
+#else// GCC version < 40700
+#if GCC_VERSION < 40600
+#define nullptr NULL
+#endif // GCC_VERSION < 40600
+#endif //non mac OS(es)
+#endif
