367c5182cfe61e9fee792a228575e47b809952b7
Gioacchino Mazzurco <gio@eigenlab.org>
2018-11-01 14:51:57 +0100

    Fix compilation with Qt older then 5.10

--- retroshare-gui/src/gui/gxschannels/GxsChannelDialog.cpp
+++ retroshare-gui/src/gui/gxschannels/GxsChannelDialog.cpp
@@ -35,6 +35,7 @@
 #include "gui/settings/rsharesettings.h"
 #include "gui/notifyqt.h"
 #include "gui/common/GroupTreeWidget.h"
+#include "util/qtthreadsutils.h"
 
 class GxsChannelGroupInfoData : public RsUserdata
 {
@@ -296,14 +297,17 @@ void GxsChannelDialog::toggleAutoDownload()
 			return;
 		}
 
-		QMetaObject::invokeMethod(this, [=]()
+		RsQThreadUtils::postToObject( [=]()
 		{
 			/* Here it goes any code you want to be executed on the Qt Gui
 			 * thread, for example to update the data model with new information
 			 * after a blocking call to RetroShare API complete, note that
 			 * Qt::QueuedConnection is important!
 			 */
-		}, Qt::QueuedConnection);
+
+			std::cerr << __PRETTY_FUNCTION__ << " Has been executed on GUI "
+			          << "thread but was scheduled by async thread" << std::endl;
+		}, this );
 	});
 }
 
