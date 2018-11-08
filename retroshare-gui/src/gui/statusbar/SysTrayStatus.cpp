1ca139f8585a4b9c92e487c4b245518604f15742
phikhong <phikhong369@gmail.com>
2018-11-08 16:38:56 +0700

    update traymenu icon

--- retroshare-gui/src/gui/statusbar/SysTrayStatus.cpp
+++ retroshare-gui/src/gui/statusbar/SysTrayStatus.cpp
@@ -25,9 +25,9 @@
 
 #include "SysTrayStatus.h"
 
-#define IMAGE_NOONLINE          ":/chat/img/p2p_0.png"              //d:replace icon
-#define IMAGE_ONEONLINE         ":/chat/img/p2p_1.png"              //d:replace icon
-#define IMAGE_TWOONLINE         ":/chat/img/p2p_2.png"              //d:replace icon
+#define IMAGE_NOONLINE          ":/chat/img/network-diagram_0.png"              //d:replace icon
+#define IMAGE_ONEONLINE         ":/chat/img/network-diagram_1.png"              //d:replace icon
+#define IMAGE_TWOONLINE         ":/chat/img/network-diagram_2.png"              //d:replace icon
 
 SysTrayStatus::SysTrayStatus(QWidget *parent) :
   QWidget(parent)
