a008fad06c62d94530ddcdf1f7b9decc07609761
csoler <csoler@users.sourceforge.net>
2018-11-19 21:45:42 +0100

    fixed bug in sizeHintRole()

--- retroshare-gui/src/gui/gxsforums/GxsForumModel.h
+++ retroshare-gui/src/gui/gxsforums/GxsForumModel.h
@@ -1,4 +1,20 @@
+#ifdef SUSPENDED_CODE
 #include "retroshare/rsgxsifacetypes.h"
+#else
+#include <string>
+#include <iostream>
+
+struct RsMsgMetaData
+{
+	std::string mMsgName ;
+	time_t mPublishTs;
+	uint32_t mMsgStatus;
+	QString mAuthorId;
+};
+
+#endif
+
+#include <QModelIndex>
 
 // This class holds the actual hierarchy of posts, represented by identifiers
 // It is responsible for auto-updating when necessary and holds a mutex to allow the Model to
@@ -29,17 +45,18 @@ public:
 
 	enum Roles{ SortRole = Qt::UserRole+1 };
 
-	int rowCount(const QModelIndex& parent = QModelIndex()) const;
-	int columnCount(const QModelIndex &parent = QModelIndex()) const;
-	bool hasChildren(const QModelIndex &parent = QModelIndex()) const;
+    int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+    bool hasChildren(const QModelIndex &parent = QModelIndex()) const override;
 
-	QModelIndex index(int row, int column, const QModelIndex & parent = QModelIndex()) const;
-	QModelIndex parent(const QModelIndex& child) const;
+    QModelIndex index(int row, int column, const QModelIndex & parent = QModelIndex()) const override;
+    QModelIndex parent(const QModelIndex& child) const override;
+    Qt::ItemFlags flags(const QModelIndex& index) const  override;
 
-	QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
-	QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
 
-	QVariant sizeHintRole(int col) const;
+    QVariant sizeHintRole(int col) const;
 	QVariant displayRole(const RsMsgMetaData& meta, int col) const;
 	QVariant userRole(const RsMsgMetaData& meta, int col) const;
 	QVariant decorationRole(const RsMsgMetaData &meta, int col) const;
@@ -47,7 +64,12 @@ public:
 	void update_posts();
 
 private:
-	static bool convertTabEntryToRefPointer(uint32_t entry,void *& ref);
+    void *getParentRef(void *ref,int& row) const;
+    void *getChildRef(void *ref,int row) const;
+    //bool hasIndex(int row,int column,const QModelIndex& parent)const;
+    int  getChildrenCount(void *ref) const;
+
+    static bool convertTabEntryToRefPointer(uint32_t entry,void *& ref);
 	static bool convertRefPointerToTabEntry(void *ref,uint32_t& entry);
 
     std::vector<ForumPostEntry> mPosts ; // store the list of posts updated from rsForums.
