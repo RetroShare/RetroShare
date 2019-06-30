86182a607b1cf3d294faa3610440b9e61ee0fa8d
hunbernd <hunbernd@outlook.com>
2019-06-30 23:20:25 +0200

    Added sticker support

--- retroshare-gui/src/gui/common/Emoticons.h
+++ retroshare-gui/src/gui/common/Emoticons.h
@@ -28,8 +28,11 @@ class Emoticons
 {
 public:
     static void load();
+    static void loadSmiley();
+    static void loadSticker(QString foldername);
 
     static void showSmileyWidget(QWidget *parent, QWidget *button, const char *slotAddMethod, bool above);
+    static void showStickerWidget(QWidget *parent, QWidget *button, const char *slotAddMethod, bool above);
 
 //    static void formatText(QString &text);
 };
