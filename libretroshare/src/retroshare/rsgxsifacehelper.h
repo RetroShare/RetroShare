39590300549ae888b4a3d84f54c5a90c4b233b6a
Gioacchino Mazzurco <gio@eigenlab.org>
2020-04-01 01:30:57 +0200

    Fix Android compilation after rebase on master

--- libretroshare/src/retroshare/rsgxsifacehelper.h
+++ libretroshare/src/retroshare/rsgxsifacehelper.h
@@ -30,6 +30,7 @@
 #include "retroshare/rsreputations.h"
 #include "rsgxsflags.h"
 #include "util/rsdeprecate.h"
+#include "util/rsdebug.h"
 
 /*!
  * This class only make method of internal members visible tu upper level to
@@ -44,7 +45,7 @@
 
 enum class TokenRequestType: uint8_t
 {
-    UNDEFINED           = 0x00,
+	__NONE              = 0x00, /// Used to detect uninitialized
     GROUP_DATA          = 0x01,
     GROUP_META          = 0x02,
     POSTS               = 0x03,
@@ -53,6 +54,7 @@ enum class TokenRequestType: uint8_t
     GROUP_STATISTICS    = 0x06,
     SERVICE_STATISTICS  = 0x07,
     NO_KILL_TYPE        = 0x08,
+	__MAX                       /// Used to detect out of range
 };
 
 class RsGxsIfaceHelper
@@ -422,9 +424,9 @@ protected:
 	        uint32_t token,
 	        std::chrono::milliseconds maxWait = std::chrono::milliseconds(20000),
 	        std::chrono::milliseconds checkEvery = std::chrono::milliseconds(100),
-            bool auto_delete_if_unsuccessful=true)
+	        bool auto_delete_if_unsuccessful=true)
 	{
-        #if defined(__ANDROID__) && (__ANDROID_API__ < 24)
+#if defined(__ANDROID__) && (__ANDROID_API__ < 24)
 		auto wkStartime = std::chrono::steady_clock::now();
 		int maxWorkAroundCnt = 10;
 LLwaitTokenBeginLabel:
@@ -432,13 +434,14 @@ LLwaitTokenBeginLabel:
 		auto timeout = std::chrono::steady_clock::now() + maxWait;
 		auto st = requestStatus(token);
 
-		while( !(st == RsTokenService::FAILED || st >= RsTokenService::COMPLETE) && std::chrono::steady_clock::now() < timeout )
+		while( !(st == RsTokenService::FAILED || st >= RsTokenService::COMPLETE)
+		       && std::chrono::steady_clock::now() < timeout )
 		{
 			std::this_thread::sleep_for(checkEvery);
 			st = requestStatus(token);
 		}
-        if(st != RsTokenService::COMPLETE && auto_delete_if_unsuccessful)
-            cancelRequest(token);
+		if(st != RsTokenService::COMPLETE && auto_delete_if_unsuccessful)
+			cancelRequest(token);
 
 #if defined(__ANDROID__) && (__ANDROID_API__ < 24)
 		/* Work around for very slow/old android devices, we don't expect this
@@ -465,35 +468,38 @@ LLwaitTokenBeginLabel:
 #endif
 
 		{
-            RS_STACK_MUTEX(mMtx);
+			RS_STACK_MUTEX(mMtx);
 			mActiveTokens.erase(token);
-        }
+		}
 
-        return st;
-    }
+		return st;
+	}
 
 private:
 	RsGxsIface& mGxs;
 	RsTokenService& mTokenService;
-    RsMutex mMtx;
+	RsMutex mMtx;
 
-    std::map<uint32_t,TokenRequestType> mActiveTokens;
+	std::map<uint32_t,TokenRequestType> mActiveTokens;
 
-    void locked_dumpTokens()
-    {
-        uint16_t service_id =  mGxs.serviceType();
+	void locked_dumpTokens()
+	{
+		uint16_t service_id =  mGxs.serviceType();
+		uint32_t count[static_cast<const size_t>(TokenRequestType::__MAX)] = {0};
 
-        uint32_t count[7] = {0};
+		RsDbg() << __PRETTY_FUNCTION__ << "Service 0x" << std::hex << service_id
+		        << " (" << rsServiceControl->getServiceName(
+		               RsServiceInfo::RsServiceInfoUIn16ToFullServiceId(service_id) )
+		        << ") this=0x" << static_cast<void*>(this)
+		        << ") Active tokens (per type): ";
 
-        std::cerr << "Service " << std::hex << service_id << std::dec
-                  << " (" << rsServiceControl->getServiceName(RsServiceInfo::RsServiceInfoUIn16ToFullServiceId(service_id))
-                  << ") this=" << std::hex << (void*)this << std::dec << ") Active tokens (per type): " ;
+		// let's count how many token of each type we've got.
+		for(auto& it: mActiveTokens) ++count[static_cast<int>(it.second)];
 
-        for(auto& it: mActiveTokens)				// let's count how many token of each type we've got.
-            ++count[static_cast<int>(it.second)];
+		for(uint32_t i=0;i<7;++i)
+			RsDbg().uStream() /* << i << ":" */ << count[i] << " ";
+		RsDbg().uStream() << std::endl;
+	}
 
-        for(uint32_t i=0;i<7;++i)
-            std::cerr << std::dec /* << i << ":" */ << count[i] << " ";
-        std::cerr << std::endl;
-    }
+	RS_SET_CONTEXT_DEBUG_LEVEL(1)
 };
