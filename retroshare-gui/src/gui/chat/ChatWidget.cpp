0ea2492ade5c643f8cb744f7c0a08c48510ed268
Pooh <RetroPooh@users.noreply.github.com>
2020-09-05 10:49:17 +0300

    fix compile

--- retroshare-gui/src/gui/chat/ChatWidget.cpp
+++ retroshare-gui/src/gui/chat/ChatWidget.cpp
@@ -1329,7 +1329,7 @@ void ChatWidget::toogle_SeachWithoutLimit()
 	bSearchWithoutLimit=!bSearchWithoutLimit;
 }
 
-void ChatWidget::eraseBannedMessages(RsGxsId *id)
+void ChatWidget::eraseBannedMessages(RsGxsId id)
 {
 	QString qsStringToFind = PERSONID + QString::fromStdString(id->toStdString());
 	QTextDocument *qtdDocument = ui->textBrowser->document();
