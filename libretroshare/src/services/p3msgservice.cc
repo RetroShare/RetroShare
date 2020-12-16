d1ba81e50fab018d9a642031743019ee09d11091
Phenom <retrosharephenom@gmail.com>
2020-12-16 16:17:44 +0100

    Fix System Message Sender

--- libretroshare/src/services/p3msgservice.cc
+++ libretroshare/src/services/p3msgservice.cc
@@ -1331,11 +1331,10 @@ bool p3MsgService::SystemMessage(const std::string &title, const std::string &me
 		return false;
 	}
 
-    const RsPeerId& ownId = mServiceCtrl->getOwnId();
 
 	RsMsgItem *msg = new RsMsgItem();
 
-	msg->PeerId(ownId);
+	msg->PeerId();// Notification == null
 
 	msg->msgFlags = 0;
 
@@ -1356,7 +1355,7 @@ bool p3MsgService::SystemMessage(const std::string &title, const std::string &me
 	msg->subject = title;
 	msg->message = message;
 
-    msg->rspeerid_msgto.ids.insert(ownId);
+	msg->rspeerid_msgto.ids.insert(mServiceCtrl->getOwnId());
 
 	processIncomingMsg(msg);
 
