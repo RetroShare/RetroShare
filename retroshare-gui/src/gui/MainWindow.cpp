a8f3a38e72ba9d379445239aaef37d75b41a294b
csoler <csoler@users.sourceforge.net>
2020-12-23 21:39:04 +0100

    added missing delete trayMenu

--- retroshare-gui/src/gui/MainWindow.cpp
+++ retroshare-gui/src/gui/MainWindow.cpp
@@ -376,6 +376,8 @@ MainWindow::~MainWindow()
     delete toasterDisable;
     delete sysTrayStatus;
     delete trayIcon;
+    delete trayMenu;
+    delete notifyMenu;
 #ifdef MESSENGER_WINDOW
     MessengerWindow::releaseInstance();
 #endif
@@ -603,7 +605,7 @@ void MainWindow::displayDiskSpaceWarning(int loc,int size_limit_mb)
 void MainWindow::createTrayIcon()
 {
     /** Tray icon Menu **/
-    QMenu *trayMenu = new QMenu(this);
+    trayMenu = new QMenu(this);
     if (sysTrayStatus) sysTrayStatus->trayMenu = trayMenu;
     QObject::connect(trayMenu, SIGNAL(aboutToShow()), this, SLOT(updateMenu()));
     toggleVisibilityAction = trayMenu->addAction(QIcon(IMAGE_RETROSHARE), tr("Show/Hide"), this, SLOT(toggleVisibilitycontextmenu()));
