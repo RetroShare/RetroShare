feba74cbef2de83f9853d0196eb90020a8fa8e86
defnax <defnax@users.noreply.github.com>
2021-02-12 21:41:17 +0100

    Some ui fixes on Plugins & Appearance Page
    * Some ui layout fixes on Plugins & Appearance Page
    * Update the help string on Activity feed

--- retroshare-gui/src/gui/NewsFeed.cpp
+++ retroshare-gui/src/gui/NewsFeed.cpp
@@ -112,15 +112,18 @@ NewsFeed::NewsFeed(QWidget *parent) : MainPage(parent), ui(new Ui::NewsFeed),
     ui->feedOptionsButton->hide();	// (csoler) Hidden until we repare the system to display a specific settings page.
 
 QString hlp_str = tr(
- " <h1><img width=\"32\" src=\":/icons/help_64.png\">&nbsp;&nbsp;News Feed</h1>                                                          \
+ " <h1><img width=\"32\" src=\":/icons/help_64.png\">&nbsp;&nbsp;Activity Feed</h1>                                                          \
    <p>The Activity Feed displays the last events on your network, sorted by the time you received them.                \
    This gives you a summary of the activity of your friends.                                                       \
    You can configure which events to show by pressing on <b>Options</b>. </p>                                      \
    <p>The various events shown are:                                                                                \
    <ul>                                                                                                         \
    <li>Connection attempts (useful to make friends with new people and control who's trying to reach you)</li> \
-   <li>Channel and Forum posts</li>                                                                            \
-   <li>New Channels and Forums you can subscribe to</li>                                                       \
+   <li>Channel, Forum and Board posts</li>                                                                            \
+   <li>Circle membership requests and invites</li>                                                                            \
+   <li>New Channels, Forums and Boards you can subscribe to</li>                                                       \
+   <li>Channel and Board comments</li>                                                                 \
+   <li>New Mail messages</li>                                                                 \
    <li>Private messages from your friends</li>                                                                 \
    </ul> </p>                                                                                                      \
  ") ;
@@ -496,11 +499,12 @@ void NewsFeed::handleSecurityEvent(std::shared_ptr<const RsEvent> event)
 
 	if (Settings->getMessageFlags() & RS_MESSAGE_CONNECT_ATTEMPT)
 		MessageComposer::addConnectAttemptMsg(e.mPgpId, e.mSslId, QString::fromStdString(det.name + "(" + det.location + ")"));
+
 }
 
 void NewsFeed::testFeeds(uint notifyFlags)
 {
-#ifdef TO_REMOVE
+
 	if (!instance) {
 		return;
 	}
@@ -520,7 +524,7 @@ void NewsFeed::testFeeds(uint notifyFlags)
 	instance->ui->feedWidget->enableCountChangedSignal(true);
 
 	instance->sendNewsFeedChanged();
-#endif
+
     std::cerr << "(EE) testFeeds() is currently disabled" << std::endl;
 }
 
