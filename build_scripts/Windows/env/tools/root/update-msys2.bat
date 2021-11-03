a9a7b556d5e60228439b97d8b6b07a19fb227494
thunder2 <thunder2@users.sourceforge.net>
2021-11-03 16:23:35 +0100

    Upgrade MSYS2 for Windows build

--- build_scripts/Windows/env/tools/root/update-msys2.bat
+++ build_scripts/Windows/env/tools/root/update-msys2.bat
@@ -8,11 +8,11 @@ if exist "%~dp0msys2\msys64" call :update 64
 goto :EOF
 
 :update
-set MSYSSH=%~dp0msys2\msys%~1\usr\bin\sh
+set MSYS2SH=%~dp0msys2\msys%~1\usr\bin\sh
 
 echo Update MSYS2 %~1
-"%MSYSSH%" -lc "pacman -Sy"
-"%MSYSSH%" -lc "pacman --noconfirm -Su"
+"%MSYS2SH%" -lc "yes | pacman --noconfirm -Syuu msys2-keyring"
+"%MSYS2SH%" -lc "pacman --noconfirm -Su"
 
 :exit
 endlocal
