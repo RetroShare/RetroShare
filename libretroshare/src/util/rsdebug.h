a9fe1ba6deaf0d4a5881312312fa243ad9ca21af
Gioacchino Mazzurco <gio@eigenlab.org>
2021-11-05 12:02:56 +0100

    Make DirStub serialization consistent on all architectures
    Thanks elRepo.io developers to detect and report this bug with detail
    https://gitlab.com/elRepo.io/elRepo.io-android/-/issues/52

--- libretroshare/src/util/rsdebug.h
+++ libretroshare/src/util/rsdebug.h
@@ -2,7 +2,7 @@
  * RetroShare debugging utilities                                              *
  *                                                                             *
  * Copyright (C) 2004-2008  Robert Fernie <retroshare@lunamutt.com>            *
- * Copyright (C) 2019-2021 Gioacchino Mazzurco <gio@eigenlab.org>              *
+ * Copyright (C) 2019-2021  Gioacchino Mazzurco <gio@eigenlab.org>             *
  * Copyright (C) 2020-2021  Asociación Civil Altermundi <info@altermundi.net>  *
  *                                                                             *
  * This program is free software: you can redistribute it and/or modify        *
