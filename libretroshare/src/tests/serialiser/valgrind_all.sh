48fcf705780b60b52541d6798037df2cd4e74778
Gioacchino Mazzurco <gio@eigenlab.org>
2022-01-10 21:37:03 +0100

    Split libretroshare BitDHT and OpenPGP-SDK into submodules

--- libretroshare/src/tests/serialiser/valgrind_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo This script will call valgrind on all executables of the current directory and launch a debugger in case it finds an error. If the script terminate with non error, then the test passes. Press enter when ready...
-read tmp
-
-for i in `find . -executable -type f | grep -v valgrind`; do
-		echo testing $i
-		valgrind --tool=memcheck --db-attach=yes ./$i
-done
--- openpgpsdk/src/openpgpsdk/opsdir.h
+++
@@ -1,6 +0,0 @@
-#ifndef OPUTIL_H
-#define OPUTIL_H
-
-int ops_open(const char* filename, int flag, int pmode);
-
-#endif
