0b5cddb2990d471eb08147e424fd0adb8b985b61
RetroPooh <RetroPooh@users.noreply.github.com>
2022-11-18 23:03:20 +0300

    tor status fix for unconfigured, add update on settings change; fix i2p out led status

--- retroshare-gui/src/gui/MainWindow.cpp
+++ retroshare-gui/src/gui/MainWindow.cpp
@@ -1628,6 +1628,10 @@ NATStatus *MainWindow::natstatusInstance()
 	return natstatus;
 }
 
+void MainWindow::torstatusReset()
+{
+	torstatus->reset();
+}
 DHTStatus *MainWindow::dhtstatusInstance()
 {
 	return dhtstatus;
