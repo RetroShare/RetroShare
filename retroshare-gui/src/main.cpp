beed6a08c828bc4a81f51ea5d52f6f1448fc74ec
csoler <csoler@users.sourceforge.net>
2023-05-04 22:30:06 +0200

    made SearchDialog to use RsEvents in order to catch turtle search results

--- retroshare-gui/src/main.cpp
+++ retroshare-gui/src/main.cpp
@@ -540,7 +540,7 @@ feenableexcept(FE_INVALID | FE_DIVBYZERO);
 	qRegisterMetaType<RsPeerId>("RsPeerId") ;
 
 	std::cerr << "connecting signals and slots" << std::endl ;
-	QObject::connect(notify,SIGNAL(gotTurtleSearchResult(qulonglong,FileDetail)),w->transfersDialog->searchDialog	,SLOT(updateFiles(qulonglong,FileDetail))) ;
+//	QObject::connect(notify,SIGNAL(gotTurtleSearchResult(qulonglong,FileDetail)),w->transfersDialog->searchDialog	,SLOT(updateFiles(qulonglong,FileDetail))) ;
 	QObject::connect(notify,SIGNAL(deferredSignatureHandlingRequested()),notify,SLOT(handleSignatureEvent()),Qt::QueuedConnection) ;
 	QObject::connect(notify,SIGNAL(chatLobbyTimeShift(int)),notify,SLOT(handleChatLobbyTimeShift(int)),Qt::QueuedConnection) ;
 	QObject::connect(notify,SIGNAL(diskFull(int,int))						,w                   		,SLOT(displayDiskSpaceWarning(int,int))) ;
