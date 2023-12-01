6fffb322ae4557f261da5066d6b070c1627e042f
defnax <9952056+defnax@users.noreply.github.com>
2023-12-01 22:26:44 +0100

    Improved makeOSXPackage.sh file
    * Added to ship sounds dir
    * Added CFBundleVersion & CFBundleShortVersionString string
    * Added to get Git head string for package naming

--- build_scripts/OSX/makeOSXPackage.sh
+++ build_scripts/OSX/makeOSXPackage.sh
@@ -17,6 +17,11 @@ rm -rf moc
 rm -rf obj
 rm -rf qrc
 
+# This sets the CFBundleVersion & CFBundleShortVersionString string
+#/usr/libexec/PlistBuddy -c "Delete :CFBundleGetInfoString" retroshare.app/Contents/Info.plist
+/usr/libexec/PlistBuddy -c "Add :CFBundleVersion string $RSVERSION" retroshare.app/Contents/Info.plist
+/usr/libexec/PlistBuddy -c "Add :CFBundleShortVersionString string $RSVERSION" retroshare.app/Contents/Info.plist
+
 # This automatically creates retroshare.dmg
 
 echo "Creating dmg archive..."
@@ -24,8 +29,10 @@ macdeployqt retroshare.app -dmg
 
 DATE=`date +"%m-%d-%Y"`
 MACVERSION=`sw_vers -productVersion`
+#RSVERSION=`git describe --abbrev=0 --tags`
+GITHEAD=`git rev-parse --short HEAD`
 
-mv $APP.dmg "$APP-$RSVERSION-$DATE-MacOS-$MACVERSION-$QTVERSION.dmg"
+mv $APP.dmg "$APP-$RSVERSION-$GITHEAD-$DATE-$MACVERSION-$QTVERSION.dmg"
 
 # You can use the appdmg command line app to create your dmg file if
 # you want to use a custom background and icon arrangement. I'm still
@@ -36,6 +43,7 @@ mv $APP.dmg "$APP-$RSVERSION-$DATE-MacOS-$MACVERSION-$QTVERSION.dmg"
 # Copy other project files
 cp "../../libbitdht/src/bitdht/bdboot.txt" "retroshare.app/Contents/Resources/"
 cp "../../plugins/FeedReader/lib/libFeedReader.dylib" "retroshare.app/Contents/Resources/"
+cp -R "sounds" "retroshare.app/Contents/Resources/sounds"
 
 # cp "${project_dir}/README.md" "README.md"
 # cp "${project_dir}/LICENSE" "LICENSE"
