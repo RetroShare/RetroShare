f5d98d28b1889a2a972857f85f4da39386640b20
jolavillette <jolavillette@gmail.com>
2025-12-21 10:26:46 +0100

    Restore graph view and use O(N) QHash lookup for the table view

--- retroshare-gui/src/gui/statistics/RttStatistics.h
+++ retroshare-gui/src/gui/statistics/RttStatistics.h
@@ -18,28 +18,55 @@
  *                                                                             *
  *******************************************************************************/
 
+/*******************************************************************************
+ * gui/statistics/RttStatistics.h                                              *
+ *******************************************************************************/
+
 #pragma once
 
-#include <QTreeWidget>
+#include <QPoint>
 #include <QTimer>
+#include <QTreeWidget>
 #include <QHeaderView>
+#include <retroshare/rsrtt.h>
 #include <retroshare-gui/RsAutoUpdatePage.h>
+
 #include "ui_RttStatistics.h"
+#include <gui/common/RSGraphWidget.h>
+
+class RttStatisticsWidget ;
+
+class RttGraphSource: public RSGraphSource
+{
+public:
+    RttGraphSource() {}
+
+    virtual void getValues(std::map<std::string,float>& vals) const ;
+    virtual QString unitName() const ;
+    virtual QString displayName(int i) const ;
+};
+
+class RttStatisticsGraph: public RSGraphWidget
+{
+public:
+    RttStatisticsGraph(QWidget *parent);
+};
 
 class RttStatistics: public MainPage, public Ui::RttStatistics
 {
     Q_OBJECT
 
 public:
-    RttStatistics(QWidget *parent = NULL);
+    RttStatistics(QWidget *parent = NULL) ;
     ~RttStatistics();
 
 private slots:
-    void updateRttValues();
+    void updateRttValues(); // Slot for table update
 
 private:
     void processSettings(bool bLoad);
     bool m_bProcessSettings;
 
-    QTimer *m_timer;
-};
+    RttStatisticsGraph *_tst_CW ;
+    QTimer *m_timer; // Timer for the table
+} ;
