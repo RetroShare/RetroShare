01c2ab4cbb3a7a08c21d3eacb29c2347df01c40e
jolavillette <jolavillette@gmail.com>
2026-02-18 17:56:46 +0100

    Add Ignore User button to Security Feed and implement Ignored Users list management

--- retroshare-gui/src/gui/feeds/SecurityItem.cpp
+++ retroshare-gui/src/gui/feeds/SecurityItem.cpp
@@ -37,6 +37,7 @@
 
 #include <retroshare/rsmail.h>
 #include <retroshare/rspeers.h>
+#include "pqi/authssl.h"
 
 /*****
  * #define DEBUG_ITEM 1
@@ -54,6 +55,12 @@ SecurityItem::SecurityItem(FeedHolder *parent, uint32_t feedId, const RsPgpId &g
 	chatButton->hide();
 	removeFriendButton->setEnabled(false);
 	removeFriendButton->hide();
+	banButton->hide();
+    
+    QPalette pal = banButton->palette();
+    pal.setColor(QPalette::ButtonText, Qt::red);
+    banButton->setPalette(pal);
+
 	peerDetailsButton->setEnabled(false);
 	friendRequesttoolButton->hide();
 	requestLabel->hide();
@@ -68,6 +75,7 @@ SecurityItem::SecurityItem(FeedHolder *parent, uint32_t feedId, const RsPgpId &g
 	//connect( quickmsgButton, SIGNAL( clicked( ) ), this, SLOT( sendMsg() ) );
 
 	connect( removeFriendButton, SIGNAL(clicked()), this, SLOT(removeFriend()));
+	connect( banButton, SIGNAL(clicked()), this, SLOT(banUser()));
 	connect( peerDetailsButton, SIGNAL(clicked()), this, SLOT(peerDetails()));
 	connect( friendRequesttoolButton, SIGNAL(clicked()), this, SLOT(friendRequest()));
 
@@ -241,6 +249,7 @@ void SecurityItem::updateItem()
 
 			removeFriendButton->setEnabled(false);
 			removeFriendButton->hide();
+			banButton->show();
 			peerDetailsButton->setEnabled(false);
 			
             if(mType == RsFeedTypeFlags::RS_FEED_ITEM_SEC_BAD_CERTIFICATE)
@@ -306,6 +315,7 @@ void SecurityItem::updateItem()
 			requestLabel->hide();
 			removeFriendButton->setEnabled(true);
 			removeFriendButton->show();
+			banButton->hide();
 		}
 		else
 		{
@@ -322,6 +332,7 @@ void SecurityItem::updateItem()
 			}
 			removeFriendButton->setEnabled(false);
 			removeFriendButton->hide();
+			banButton->show();
 		}
 
 		//quickmsgButton->show();
@@ -375,6 +386,20 @@ void SecurityItem::removeFriend()
 		rsPeers->removeFriend(mGpgId);
 	}
 }
+
+void SecurityItem::banUser()
+{
+    // Confirmation dialog
+    QMessageBox::StandardButton reply;
+    reply = QMessageBox::question(this, tr("Ignore User"), tr("Are you sure you want to ignore this user? They will be added to the Ignored Users list and you will not receive further notifications from them. You can manage the list in Preferences -> Notify."),
+                                  QMessageBox::Yes|QMessageBox::No);
+    if (reply == QMessageBox::Yes) {
+        AuthSSL::instance().addNotifyDeny(mGpgId, mSslCn);
+        // Remove this item from the feed as it is now handled
+        removeItem(); 
+    }
+}
+
 void SecurityItem::friendRequest()
 {
 #ifdef DEBUG_ITEM
