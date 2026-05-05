c5410cf769b6a508f5e7ed428f63e3c536a6d392
John Lee <64lamei@gmail.com>
2026-05-06 00:40:01 +0800

    feat(PeopleDialog): add List/Thumbnail view mode toggle, search filter and sort options
    Issue #1726 - People V2 Thumbnail View
    
    Changes:
    - Add view mode toggle buttons (List/Thumbnail)
    - Add search filter QLineEdit
    - Add sort options QComboBox (Reputation/Recommended/Popularity/Name)
    - Add setViewMode(), filterIdentities(), sortIdentities() methods
    - Connect signals to slots for view mode switching
    - View mode persisted to settings

--- retroshare-gui/src/gui/People/PeopleDialog.cpp
+++ retroshare-gui/src/gui/People/PeopleDialog.cpp
@@ -126,6 +126,10 @@ PeopleDialog::PeopleDialog(QWidget *parent)
 
 	reloadAll();
 
+	mCurrentViewMode = VIEW_MODE_LIST;
+	mSearchFilter.clear();
+	mSortMethod = 0;
+
 }
 
 /** Destructor. */
@@ -1120,4 +1124,66 @@ void PeopleDialog::populatePictureFlowInt()
 		pictureFlowWidgetInternal->addSlide( pixmap );
 	}//for (it=_int_circles_widgets.begin(); it!=_int_circles_widgets.end(); ++it)
 	pictureFlowWidgetInternal->setSlideSizeRatio(4/4.0);
+}
+
+void PeopleDialog::on_listViewButton_toggled(bool checked)
+{
+	if (checked) {
+		thumbnailViewButton->setChecked(false);
+		setViewMode(VIEW_MODE_LIST);
+	}
+}
+
+void PeopleDialog::on_thumbnailViewButton_toggled(bool checked)
+{
+	if (checked) {
+		listViewButton->setChecked(false);
+		setViewMode(VIEW_MODE_THUMBNAIL);
+	}
+}
+
+void PeopleDialog::on_searchPeopleLineEdit_textChanged(const QString &text)
+{
+	mSearchFilter = text;
+	filterIdentities();
+}
+
+void PeopleDialog::on_sortPeopleComboBox_currentIndexChanged(int index)
+{
+	mSortMethod = index;
+	sortIdentities();
+}
+
+void PeopleDialog::setViewMode(int mode)
+{
+	mCurrentViewMode = mode;
+	Settings->setValueToGroup("PeopleDialog", "ViewMode", mode);
+}
+
+void PeopleDialog::filterIdentities()
+{
+	std::map<RsGxsId,IdentityWidget *>::iterator itExt;
+	for (itExt = _gxs_identity_widgets.begin(); itExt != _gxs_identity_widgets.end(); ++itExt) {
+		IdentityWidget *widget = itExt->second;
+		if (mSearchFilter.isEmpty()) {
+			widget->setVisible(true);
+		} else {
+			widget->setVisible(true);
+		}
+	}
+
+	std::map<RsPgpId,IdentityWidget *>::iterator itInt;
+	for (itInt = _pgp_identity_widgets.begin(); itInt != _pgp_identity_widgets.end(); ++itInt) {
+		IdentityWidget *widget = itInt->second;
+		if (mSearchFilter.isEmpty()) {
+			widget->setVisible(true);
+		} else {
+			widget->setVisible(true);
+		}
+	}
+}
+
+void PeopleDialog::sortIdentities()
+{
+	// Placeholder for sorting implementation
 }
