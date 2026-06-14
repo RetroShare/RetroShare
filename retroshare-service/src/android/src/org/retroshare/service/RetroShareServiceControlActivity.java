912e096bdaa3f48b2f8a9c61f61a05e3956f87cd
Gioacchino Mazzurco <gio@retroshare.cc>
2026-06-14 16:10:56 +0200

    Fix G10h4ck contributions copyright attribution

--- retroshare-service/src/android/src/org/retroshare/service/RetroShareServiceControlActivity.java
+++ retroshare-service/src/android/src/org/retroshare/service/RetroShareServiceControlActivity.java
@@ -1,6 +1,6 @@
 /*
  * RetroShare
- * Copyright (C) 2016-2021  Gioacchino Mazzurco <gio@altermundi.net>
+ * Copyright (C) 2016-2021  Gioacchino Mazzurco <gio@retroshare.cc>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
